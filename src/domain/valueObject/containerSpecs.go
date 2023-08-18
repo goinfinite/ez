a3f8a9fd304f994ef9bb7c5f3c7298f3f29da2bb
ntorga <northontorga@gmail.com>
2023-08-18 13:08:24 +0000

    feat: add missing getters as string

--- src/domain/valueObject/containerSpecs.go
+++ src/domain/valueObject/containerSpecs.go
@@ -1,5 +1,7 @@
 package valueObject
 
+import "strconv"
+
 type ContainerSpecs struct {
 	CpuCores     float64 `json:"cpuCores"`
 	MemoryBytes  Byte    `json:"memoryBytes"`
@@ -16,4 +18,28 @@ func NewContainerSpecs(
 		MemoryBytes:  memoryBytes,
 		StorageBytes: storageBytes,
 	}
+}
+
+func (specs ContainerSpecs) GetCpuCores() float64 {
+	return specs.CpuCores
+}
+
+func (specs ContainerSpecs) GetMemoryBytes() Byte {
+	return specs.MemoryBytes
+}
+
+func (specs ContainerSpecs) GetStorageBytes() Byte {
+	return specs.StorageBytes
+}
+
+func (specs ContainerSpecs) GetCpuCoresAsString() string {
+	return strconv.FormatFloat(specs.CpuCores, 'f', 1, 64)
+}
+
+func (specs ContainerSpecs) GetMemoryAsString() string {
+	return strconv.FormatInt(specs.MemoryBytes.Get(), 10)
+}
+
+func (specs ContainerSpecs) GetStorageAsString() string {
+	return strconv.FormatInt(specs.StorageBytes.Get(), 10)
 }
--- src/domain/valueObject/portBinding.go
+++ src/domain/valueObject/portBinding.go
@@ -1,5 +1,7 @@
 package valueObject
 
+import "strconv"
+
 type PortBinding struct {
 	Protocol      NetworkProtocol `json:"protocol"`
 	HostPort      uint64          `json:"hostPort"`
@@ -16,4 +18,24 @@ func NewPortBinding(
 		HostPort:      hostPort,
 		ContainerPort: containerPort,
 	}
+}
+
+func (portBinding PortBinding) GetProtocol() NetworkProtocol {
+	return portBinding.Protocol
+}
+
+func (portBinding PortBinding) GetHostPort() uint64 {
+	return portBinding.HostPort
+}
+
+func (portBinding PortBinding) GetContainerPort() uint64 {
+	return portBinding.ContainerPort
+}
+
+func (portBinding PortBinding) GetHostPortAsString() string {
+	return strconv.FormatUint(portBinding.HostPort, 10)
+}
+
+func (portBinding PortBinding) GetContainerPortAsString() string {
+	return strconv.FormatUint(portBinding.ContainerPort, 10)
 }
