7f84fbf1967a9f0a2aafc74aa28dbd4fb1a3400d
ntorga <northontorga@gmail.com>
2023-08-22 12:35:45 +0000

    feat: add add container unit test

---
+++ src/infra/containerCmdRepo_test.go
@@ -0,0 +1,58 @@
+package infra
+
+import (
+	"testing"
+
+	testHelpers "github.com/speedianet/sfm/src/devUtils"
+	"github.com/speedianet/sfm/src/domain/dto"
+	"github.com/speedianet/sfm/src/domain/valueObject"
+)
+
+func TestContainerCmdRepo(t *testing.T) {
+	testHelpers.LoadEnvVars()
+
+	t.Run("AddContainer", func(t *testing.T) {
+		repo := ContainerCmdRepo{}
+
+		portBindings := []valueObject.PortBinding{
+			valueObject.NewPortBinding(
+				valueObject.NewNetworkProtocolPanic("tcp"),
+				8080,
+				8080,
+			),
+			valueObject.NewPortBinding(
+				valueObject.NewNetworkProtocolPanic("tcp"),
+				8443,
+				8443,
+			),
+		}
+
+		restartPolicy := valueObject.NewContainerRestartPolicyPanic("unless-stopped")
+
+		baseSpecs := valueObject.NewContainerSpecs(
+			1,
+			valueObject.Byte(1073741824),
+		)
+
+		envs := []valueObject.ContainerEnv{
+			valueObject.NewContainerEnvPanic("SFM_ENV1=testing"),
+			valueObject.NewContainerEnvPanic("SFM_ENV2=testing"),
+		}
+
+		addContainer := dto.NewAddContainer(
+			valueObject.NewFqdnPanic("speedia.net"),
+			valueObject.NewContainerImgAddressPanic("docker.io/speedia/sam:latest"),
+			&portBindings,
+			&restartPolicy,
+			nil,
+			&baseSpecs,
+			nil,
+			&envs,
+		)
+
+		err := repo.Add(addContainer)
+		if err != nil {
+			t.Errorf("Expected no error, got %v", err)
+		}
+	})
+}
