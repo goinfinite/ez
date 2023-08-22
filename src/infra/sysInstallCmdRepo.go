185b49ba1b208bad7ca674e1f9db338983c10109
ntorga <northontorga@gmail.com>
2023-08-22 14:13:00 +0000

    feat: add AddDataDisk impl

--- src/infra/sysInstallCmdRepo.go
+++ src/infra/sysInstallCmdRepo.go
@@ -1,6 +1,7 @@
 package infra
 
 import (
+	"errors"
 	"os"
 
 	infraHelper "github.com/speedianet/sfm/src/infra/helper"
@@ -34,6 +35,81 @@ func (repo SysInstallCmdRepo) Install() error {
 	return nil
 }
 
+func (repo SysInstallCmdRepo) getAdditionalDisk() (string, error) {
+	primaryPart, err := infraHelper.RunCmd(
+		"bash",
+		"-c",
+		"mount | awk '/on / type/{print $1}'",
+	)
+	if err != nil {
+		return "", errors.New("GetPrimaryPartError")
+	}
+
+	primaryDiskId, err := infraHelper.RunCmd(
+		"bash",
+		"-c",
+		"lsblk "+primaryPart+" -n --output PKNAME",
+	)
+	if err != nil {
+		return "", errors.New("GetPrimaryDiskIdError")
+	}
+
+	additionalDisk, err := infraHelper.RunCmd(
+		"bash",
+		"-c",
+		"lsblk -ndp -e 7 --output KNAME | grep -v '/dev/"+primaryDiskId+"' | head -n1",
+	)
+	if err != nil {
+		return "", errors.New("GetAddDiskError")
+	}
+
+	return additionalDisk, nil
+}
+
 func (repo SysInstallCmdRepo) AddDataDisk() error {
+	addDisk, err := repo.getAdditionalDisk()
+	if err != nil {
+		return err
+	}
+
+	addDiskFilesystem, err := infraHelper.RunCmd(
+		"lsblk",
+		addDisk,
+		"-n",
+		"--output",
+		"FSTYPE",
+	)
+	if err != nil {
+		return errors.New("GetAddDiskFilesystemError")
+	}
+
+	if addDiskFilesystem != "" {
+		return errors.New("AddDiskCannotHaveFilesystem")
+	}
+
+	_, err = infraHelper.RunCmd("mkfs.xfs", addDisk)
+	if err != nil {
+		return errors.New("MkfsDataDiskFailed")
+	}
+
+	_, err = infraHelper.RunCmd("mkdir", "/var/data")
+	if err != nil {
+		return errors.New("MkdirDataDirFailed")
+	}
+
+	_, err = infraHelper.RunCmd(
+		"bash",
+		"-c",
+		"echo '"+addDisk+" /var/data xfs defaults,uquota 0 0' >> /etc/fstab",
+	)
+	if err != nil {
+		return errors.New("AddDataDiskToFsTabFailed")
+	}
+
+	_, err = infraHelper.RunCmd("mount", "-a")
+	if err != nil {
+		return errors.New("MountDataDiskFailed")
+	}
+
 	return nil
 }
