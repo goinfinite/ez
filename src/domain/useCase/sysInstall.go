cfd9b6347bb2c3f68ade8049541ffc1be8a72174
ntorga <northontorga@gmail.com>
2023-08-22 19:15:20 +0000

    feat: add sfm to systemd after install

--- src/domain/useCase/sysInstall.go
+++ src/domain/useCase/sysInstall.go
@@ -19,6 +19,8 @@ func SysInstall(
 		return errors.New("SysAlreadyInstalled")
 	}
 
+	svcInstallName := valueObject.NewSvcNamePanic("sys-install-continue")
+
 	if !isInstalled {
 		err := sysInstallCmdRepo.Install()
 		if err != nil {
@@ -29,26 +31,38 @@ func SysInstall(
 			valueObject.NewServerLogLevelPanic("info"),
 			valueObject.NewServerLogOperationPanic("sys-install"),
 			valueObject.NewServerLogPayloadPanic(
-				"Packages installed, the system will now reboot "+
-					"and then continue the process.",
+				"Packages installed. The server will now reboot "+
+					"and then continue the process automatically.",
 			),
 		)
-		serverCmdRepo.AddOneTimerSvc("sys-install-continue", "sfm sys-install")
+		serverCmdRepo.AddOneTimerSvc(
+			svcInstallName,
+			valueObject.NewSvcCmdPanic("/speedia/sfm sys-install"),
+		)
 		serverCmdRepo.SendServerMessage("Server is rebooting!")
 		serverCmdRepo.Reboot()
 	}
 
 	serverCmdRepo.SendServerMessage(
-		"[Elevator Music ♬] SFM is still installing... " +
+		"[elevator music ♬] SFM is still installing... " +
 			"please await and don't interact with the server.",
 	)
-	serverCmdRepo.DeleteOneTimerSvc("sys-install-continue")
+	serverCmdRepo.DeleteOneTimerSvc(svcInstallName)
 	err := sysInstallCmdRepo.AddDataDisk()
 	if err != nil {
 		return err
 	}
 
-	successMessage := "SFM has been installed and is now ready to be used."
+	err = serverCmdRepo.AddSvc(
+		valueObject.NewSvcNamePanic("sfm"),
+		valueObject.NewSvcCmdPanic("/speedia/sfm serve"),
+	)
+	if err != nil {
+		return err
+	}
+
+	successMessage := "SFM has been installed and is running. " +
+		"The server is now ready to be used."
 	serverCmdRepo.AddServerLog(
 		valueObject.NewServerLogLevelPanic("info"),
 		valueObject.NewServerLogOperationPanic("sys-install"),
