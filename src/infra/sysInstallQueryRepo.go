3d5cd8e3141529e3a4e9490593970dcedfeff226
ntorga <northontorga@gmail.com>
2023-08-22 17:34:32 +0000

    feat: impl IsDataDiskMounted

--- src/infra/sysInstallQueryRepo.go
+++ src/infra/sysInstallQueryRepo.go
@@ -1,11 +1,20 @@
 package infra
 
-import infraHelper "github.com/speedianet/sfm/src/infra/helper"
+import (
+	"os"
+
+	infraHelper "github.com/speedianet/sfm/src/infra/helper"
+)
 
 type SysInstallQueryRepo struct {
 }
 
 func (repo SysInstallQueryRepo) IsInstalled() bool {
 	_, err := infraHelper.GetFilePathWithMatch("/etc/pam.d/", "system-auth")
+	return err == nil
+}
+
+func (repo SysInstallQueryRepo) IsDataDiskMounted() bool {
+	_, err := os.Stat("/var/data")
 	return err == nil
 }
