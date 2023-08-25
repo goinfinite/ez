fec3be4ddb5eb19029521b19dbf0214ca410e9cf
ntorga <northontorga@gmail.com>
2023-08-25 21:05:47 +0000

    fix: rename account id and add dates

--- src/domain/entity/account.go
+++ src/domain/entity/account.go
@@ -3,25 +3,31 @@ package entity
 import "github.com/speedianet/sfm/src/domain/valueObject"
 
 type Account struct {
-	Username   valueObject.Username     `json:"username"`
-	AccountId  valueObject.AccountId    `json:"id"`
+	Id         valueObject.AccountId    `json:"id"`
 	GroupId    valueObject.GroupId      `json:"groupId"`
+	Username   valueObject.Username     `json:"username"`
 	Quota      valueObject.AccountQuota `json:"quota"`
 	QuotaUsage valueObject.AccountQuota `json:"quotaUsage"`
+	CreatedAt  valueObject.UnixTime     `json:"createdAt"`
+	UpdatedAt  valueObject.UnixTime     `json:"updatedAt"`
 }
 
 func NewAccount(
-	username valueObject.Username,
-	accountId valueObject.AccountId,
+	id valueObject.AccountId,
 	groupId valueObject.GroupId,
+	username valueObject.Username,
 	quota valueObject.AccountQuota,
 	quotaUsage valueObject.AccountQuota,
+	createdAt valueObject.UnixTime,
+	updatedAt valueObject.UnixTime,
 ) Account {
 	return Account{
-		Username:   username,
-		AccountId:  accountId,
+		Id:         id,
 		GroupId:    groupId,
+		Username:   username,
 		Quota:      quota,
 		QuotaUsage: quotaUsage,
+		CreatedAt:  createdAt,
+		UpdatedAt:  updatedAt,
 	}
 }
