9904a2adc86d981d00b55e85b5b4ffbb05e43d9c
ntorga <northontorga@gmail.com>
2023-09-07 20:28:14 +0000

    feat: add auto migrate

--- src/infra/db/dbService.go
+++ src/infra/db/dbService.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/glebarez/sqlite"
+	dbModel "github.com/speedianet/sfm/src/infra/db/model"
 	"gorm.io/gorm"
 )
 
@@ -11,6 +12,15 @@ func DatabaseService() (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open("/var/speedia/sfm.db"), &gorm.Config{})
 	if err != nil {
 		return nil, errors.New("DatabaseConnectionError")
+	}
+
+	err = db.AutoMigrate(
+		&dbModel.Account{},
+		&dbModel.AccountQuota{},
+		&dbModel.AccountQuotaUsage{},
+	)
+	if err != nil {
+		return nil, errors.New("DatabaseMigrationError")
 	}
 
 	return db, nil
