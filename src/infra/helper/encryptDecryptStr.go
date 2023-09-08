cda27a6577511fbe6d3115a6fbd412dee4155f25
ntorga <northontorga@gmail.com>
2023-09-08 19:31:33 +0000

    feat: add decrypt str helper

--- src/infra/helper/encryptDecryptStr.go
+++ src/infra/helper/encryptDecryptStr.go
@@ -36,3 +36,31 @@ func EncryptStr(secretKey string, plainText string) (string, error) {
 
 	return base64.StdEncoding.EncodeToString(cipherText), nil
 }
+
+func DecryptStr(secretKey string, encryptedText string) (string, error) {
+	apiKeyDecoded, err := base64.StdEncoding.DecodeString(encryptedText)
+	if err != nil {
+		return "", errors.New("DecryptDecodingError")
+	}
+	if len(apiKeyDecoded) < aes.BlockSize {
+		return "", errors.New("DecryptDecodedTooShort")
+	}
+
+	secretKeyBytes, err := base64.RawURLEncoding.DecodeString(secretKey)
+	if err != nil {
+		return "", errors.New("DecryptSecretDecodingError")
+	}
+
+	block, err := aes.NewCipher(secretKeyBytes)
+	if err != nil {
+		return "", errors.New("DecryptCipherError")
+	}
+
+	apiKeyDecryptedBinary := make([]byte, len(apiKeyDecoded)-aes.BlockSize)
+	iv := apiKeyDecoded[:aes.BlockSize]
+
+	stream := cipher.NewCTR(block, iv)
+	stream.XORKeyStream(apiKeyDecryptedBinary, apiKeyDecoded[aes.BlockSize:])
+
+	return string(apiKeyDecryptedBinary), nil
+}
