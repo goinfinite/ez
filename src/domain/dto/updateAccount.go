4279b3938bff3e70534375b46b9c3441820b4b97
ntorga <northontorga@gmail.com>
2023-09-11 14:22:25 +0000

    feat: add quota to update acc dto

--- src/domain/dto/updateAccount.go
+++ src/domain/dto/updateAccount.go
@@ -3,19 +3,22 @@ package dto
 import "github.com/speedianet/sfm/src/domain/valueObject"
 
 type UpdateAccount struct {
-	AccountId          valueObject.AccountId `json:"accountId"`
-	Password           *valueObject.Password `json:"password"`
-	ShouldUpdateApiKey *bool                 `json:"shouldUpdateApiKey"`
+	AccountId          valueObject.AccountId     `json:"accountId"`
+	Password           *valueObject.Password     `json:"password"`
+	ShouldUpdateApiKey *bool                     `json:"shouldUpdateApiKey"`
+	Quota              *valueObject.AccountQuota `json:"quota"`
 }
 
 func NewUpdateAccount(
 	accountId valueObject.AccountId,
 	password *valueObject.Password,
 	shouldUpdateApiKey *bool,
+	quota *valueObject.AccountQuota,
 ) UpdateAccount {
 	return UpdateAccount{
 		AccountId:          accountId,
 		Password:           password,
 		ShouldUpdateApiKey: shouldUpdateApiKey,
+		Quota:              quota,
 	}
 }
