2b6efd5c65ba021ccb4e9378e62b50048f2626ae
ntorga <northontorga@gmail.com>
2023-09-12 19:40:27 +0000

    fix: improve env vars on run cmd as user

--- src/infra/helper/runCmd.go
+++ src/infra/helper/runCmd.go
@@ -3,7 +3,9 @@ package infraHelper
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"os/exec"
+	"os/user"
 	"strings"
 	"syscall"
 
@@ -50,11 +52,32 @@ func RunCmdAsUser(
 	command string,
 	args ...string,
 ) (string, error) {
+	userInfo, err := user.LookupId(accId.String())
+	if err != nil {
+		return "", errors.New("AccountIdNotFound")
+	}
+
+	gId, err := valueObject.NewGroupId(userInfo.Gid)
+	if err != nil {
+		return "", errors.New("GroupIdNotFound")
+	}
+
 	execCmd := exec.Command(command, args...)
 	execCmd.SysProcAttr = &syscall.SysProcAttr{}
 	execCmd.SysProcAttr.Credential = &syscall.Credential{
 		Uid: uint32(accId),
+		Gid: uint32(gId),
 	}
-
+	execCmd.Env = []string{
+		"HOME=" + userInfo.HomeDir,
+		"LOGNAME=" + userInfo.Username,
+		"USER=" + userInfo.Username,
+		"SHELL=/usr/sbin/nologin",
+		"PATH=/usr/sbin:/usr/bin:/sbin:/bin",
+		"MAIL=/var/mail/" + userInfo.Username,
+		"LANG=en_US.UTF-8",
+		"PWD=" + userInfo.HomeDir,
+		"TERM=xterm-256color",
+	}
 	return runExecCmd(execCmd)
 }
