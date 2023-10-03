f0f9aa526cf0dbeb52bf912aecf5a03c7daec3a3
ntorga <northontorga@gmail.com>
2023-10-03 12:48:04 +0000

    fix: update container dtos after resource profile

--- src/domain/dto/addContainer.go
+++ src/domain/dto/addContainer.go
@@ -3,37 +3,34 @@ package dto
 import "github.com/speedianet/sfm/src/domain/valueObject"
 
 type AddContainer struct {
-	AccountId     valueObject.AccountId               `json:"accountId"`
-	Hostname      valueObject.Fqdn                    `json:"hostname"`
-	Image         valueObject.ContainerImgAddress     `json:"image"`
-	PortBindings  *[]valueObject.PortBinding          `json:"portBindings"`
-	RestartPolicy *valueObject.ContainerRestartPolicy `json:"restartPolicy"`
-	Entrypoint    *valueObject.ContainerEntrypoint    `json:"entrypoint"`
-	BaseSpecs     *valueObject.ContainerSpecs         `json:"baseSpecs"`
-	MaxSpecs      *valueObject.ContainerSpecs         `json:"maxSpecs"`
-	Envs          *[]valueObject.ContainerEnv         `json:"envs"`
+	AccountId         valueObject.AccountId               `json:"accountId"`
+	Hostname          valueObject.Fqdn                    `json:"hostname"`
+	Image             valueObject.ContainerImgAddress     `json:"image"`
+	PortBindings      []valueObject.PortBinding           `json:"portBindings"`
+	RestartPolicy     *valueObject.ContainerRestartPolicy `json:"restartPolicy"`
+	Entrypoint        *valueObject.ContainerEntrypoint    `json:"entrypoint"`
+	ResourceProfileId *valueObject.ResourceProfileId      `json:"resourceProfileId"`
+	Envs              []valueObject.ContainerEnv          `json:"envs"`
 }
 
 func NewAddContainer(
 	accountId valueObject.AccountId,
 	hostname valueObject.Fqdn,
 	image valueObject.ContainerImgAddress,
-	portBindings *[]valueObject.PortBinding,
+	portBindings []valueObject.PortBinding,
 	restartPolicy *valueObject.ContainerRestartPolicy,
 	entrypoint *valueObject.ContainerEntrypoint,
-	baseSpecs *valueObject.ContainerSpecs,
-	maxSpecs *valueObject.ContainerSpecs,
-	envs *[]valueObject.ContainerEnv,
+	resourceProfileId *valueObject.ResourceProfileId,
+	envs []valueObject.ContainerEnv,
 ) AddContainer {
 	return AddContainer{
-		AccountId:     accountId,
-		Hostname:      hostname,
-		Image:         image,
-		PortBindings:  portBindings,
-		RestartPolicy: restartPolicy,
-		Entrypoint:    entrypoint,
-		BaseSpecs:     baseSpecs,
-		MaxSpecs:      maxSpecs,
-		Envs:          envs,
+		AccountId:         accountId,
+		Hostname:          hostname,
+		Image:             image,
+		PortBindings:      portBindings,
+		RestartPolicy:     restartPolicy,
+		Entrypoint:        entrypoint,
+		ResourceProfileId: resourceProfileId,
+		Envs:              envs,
 	}
 }
--- src/domain/dto/updateContainer.go
+++ src/domain/dto/updateContainer.go
@@ -3,25 +3,22 @@ package dto
 import "github.com/speedianet/sfm/src/domain/valueObject"
 
 type UpdateContainer struct {
-	AccountId   valueObject.AccountId       `json:"accountId"`
-	ContainerId valueObject.ContainerId     `json:"id"`
-	Status      bool                        `json:"status"`
-	BaseSpecs   *valueObject.ContainerSpecs `json:"baseSpecs"`
-	MaxSpecs    *valueObject.ContainerSpecs `json:"maxSpecs"`
+	AccountId         valueObject.AccountId          `json:"accountId"`
+	ContainerId       valueObject.ContainerId        `json:"id"`
+	Status            bool                           `json:"status"`
+	ResourceProfileId *valueObject.ResourceProfileId `json:"resourceProfileId"`
 }
 
 func NewUpdateContainer(
 	accountId valueObject.AccountId,
 	containerId valueObject.ContainerId,
 	status bool,
-	baseSpecs *valueObject.ContainerSpecs,
-	maxSpecs *valueObject.ContainerSpecs,
+	resourceProfileId *valueObject.ResourceProfileId,
 ) UpdateContainer {
 	return UpdateContainer{
-		AccountId:   accountId,
-		ContainerId: containerId,
-		Status:      status,
-		BaseSpecs:   baseSpecs,
-		MaxSpecs:    maxSpecs,
+		AccountId:         accountId,
+		ContainerId:       containerId,
+		Status:            status,
+		ResourceProfileId: resourceProfileId,
 	}
 }
