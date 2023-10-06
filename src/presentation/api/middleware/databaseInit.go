1c9b1aeb64b72990c7697a5ca8a2cea471369998
ntorga <northontorga@gmail.com>
2023-10-06 19:58:55 +0000

    feat: add db init api middleware

---
+++ src/presentation/api/middleware/databaseInit.go
@@ -0,0 +1,28 @@
+package apiMiddleware
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/speedianet/sfm/src/infra/db"
+)
+
+func DatabaseInit() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			dbSvc, err := db.DatabaseService()
+			if err != nil {
+				return echo.NewHTTPError(
+					http.StatusInternalServerError,
+					map[string]interface{}{
+						"status": http.StatusInternalServerError,
+						"body":   "DatabaseConnectionError",
+					})
+			}
+
+			c.Set("dbSvc", dbSvc)
+
+			return next(c)
+		}
+	}
+}
