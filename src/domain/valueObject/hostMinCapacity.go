e2883dff0e31d1c571010fbfe0af401d48b7c0b3
ntorga <northontorga@gmail.com>
2023-10-11 18:21:24 +0000

    feat: add host min capacity vo

---
+++ src/domain/valueObject/hostMinCapacity.go
@@ -0,0 +1,39 @@
+package valueObject
+
+import (
+	"errors"
+	"strconv"
+
+	voHelper "github.com/speedianet/sfm/src/domain/valueObject/helper"
+)
+
+type HostMinCapacity float64
+
+func NewHostMinCapacity(value interface{}) (HostMinCapacity, error) {
+	hmc, err := voHelper.InterfaceToFloat(value)
+	if err != nil {
+		return 0, errors.New("InvalidHostMinCapacity")
+	}
+
+	if hmc < 0 || hmc > 100 {
+		return 0, errors.New("HostMinCapacityInvalidRange")
+	}
+
+	return HostMinCapacity(hmc), nil
+}
+
+func NewHostMinCapacityPanic(value interface{}) HostMinCapacity {
+	hmc, err := NewHostMinCapacity(value)
+	if err != nil {
+		panic(err)
+	}
+	return hmc
+}
+
+func (hmc HostMinCapacity) Get() float64 {
+	return float64(hmc)
+}
+
+func (hmc HostMinCapacity) String() string {
+	return strconv.FormatFloat(float64(hmc), 'f', -1, 64)
+}
