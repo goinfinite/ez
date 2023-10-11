15c16cb7e2989cf055796c49754896def649f6d0
ntorga <northontorga@gmail.com>
2023-10-11 20:42:39 +0000

    feat: impl add r profile infra

--- src/infra/resourceProfileCmdRepo.go
+++ src/infra/resourceProfileCmdRepo.go
@@ -3,14 +3,31 @@ package infra
 import (
 	"github.com/speedianet/sfm/src/domain/dto"
 	"github.com/speedianet/sfm/src/domain/valueObject"
+	dbModel "github.com/speedianet/sfm/src/infra/db/model"
+	"gorm.io/gorm"
 )
 
 type ResourceProfileCmdRepo struct {
+	dbSvc *gorm.DB
+}
+
+func NewResourceProfileCmdRepo(dbSvc *gorm.DB) *ResourceProfileCmdRepo {
+	return &ResourceProfileCmdRepo{dbSvc: dbSvc}
 }
 
 func (repo ResourceProfileCmdRepo) Add(
 	addDto dto.AddResourceProfile,
 ) error {
+	resourceProfileModel, err := dbModel.ResourceProfile{}.ToModel(addDto)
+	if err != nil {
+		return err
+	}
+
+	err = repo.dbSvc.Create(&resourceProfileModel).Error
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
