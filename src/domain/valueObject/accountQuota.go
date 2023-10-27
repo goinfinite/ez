2a9b467a9f157985ae3ae5c1eb2a521dad43ece5
ntorga <northontorga@gmail.com>
2023-10-27 13:12:18 +0000

    feat: add NewAccountQuotaFromString

--- src/domain/valueObject/accountQuota.go
+++ src/domain/valueObject/accountQuota.go
@@ -1,5 +1,12 @@
 package valueObject
 
+import (
+	"errors"
+	"strings"
+
+	voHelper "github.com/goinfinite/fleet/src/domain/valueObject/helper"
+)
+
 type AccountQuota struct {
 	CpuCores    CpuCoresCount `json:"cpuCores"`
 	MemoryBytes Byte          `json:"memoryBytes"`
@@ -19,6 +26,48 @@ func NewAccountQuota(
 		DiskBytes:   diskBytes,
 		Inodes:      inodes,
 	}
+}
+
+func NewAccountQuotaFromString(value string) (AccountQuota, error) {
+	if value == "" {
+		return AccountQuota{}, errors.New("InvalidAccountQuotaValue")
+	}
+
+	if !strings.Contains(value, ":") {
+		return AccountQuota{}, errors.New("InvalidAccountQuotaFormat")
+	}
+
+	specParts := strings.Split(value, ":")
+	if len(specParts) != 4 {
+		return AccountQuota{}, errors.New("InvalidAccountQuotaStructure")
+	}
+
+	cpuCores, err := NewCpuCoresCount(specParts[0])
+	if err != nil {
+		return AccountQuota{}, err
+	}
+
+	memory, err := voHelper.InterfaceToUint(specParts[1])
+	if err != nil {
+		return AccountQuota{}, errors.New("InvalidMemoryLimit")
+	}
+
+	disk, err := voHelper.InterfaceToUint(specParts[2])
+	if err != nil {
+		return AccountQuota{}, errors.New("InvalidDiskLimit")
+	}
+
+	inodes, err := NewInodesCount(specParts[3])
+	if err != nil {
+		return AccountQuota{}, err
+	}
+
+	return NewAccountQuota(
+		cpuCores,
+		Byte(int64(memory)),
+		Byte(int64(disk)),
+		inodes,
+	), nil
 }
 
 func NewAccountQuotaWithDefaultValues() AccountQuota {
