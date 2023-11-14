1b41fbdcef54efc2f0c1592f81396f041d4438d0
ntorga <northontorga@gmail.com>
2023-11-14 16:26:10 +0000

    refactor: use slices instead of const for np

--- src/domain/valueObject/networkProtocol.go
+++ src/domain/valueObject/networkProtocol.go
@@ -1,37 +1,33 @@
 package valueObject
 
-import "errors"
+import (
+	"errors"
+	"slices"
+	"strings"
+)
 
 type NetworkProtocol string
 
-const (
-	tcp NetworkProtocol = "tcp"
-	udp NetworkProtocol = "udp"
-)
+var validNetworkProtocols = []string{
+	"tcp",
+	"udp",
+}
 
 func NewNetworkProtocol(value string) (NetworkProtocol, error) {
-	np := NetworkProtocol(value)
-	if !np.isValid() {
+	value = strings.TrimSpace(value)
+	value = strings.ToLower(value)
+	if !slices.Contains(validNetworkProtocols, value) {
 		return "", errors.New("InvalidNetworkProtocol")
 	}
-	return np, nil
+	return NetworkProtocol(value), nil
 }
 
 func NewNetworkProtocolPanic(value string) NetworkProtocol {
-	np := NetworkProtocol(value)
-	if !np.isValid() {
-		panic("InvalidNetworkProtocol")
+	np, err := NewNetworkProtocol(value)
+	if err != nil {
+		panic(err)
 	}
 	return np
-}
-
-func (np NetworkProtocol) isValid() bool {
-	switch np {
-	case tcp, udp:
-		return true
-	default:
-		return false
-	}
 }
 
 func (np NetworkProtocol) String() string {
