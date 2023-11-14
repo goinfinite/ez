100178858f5ac1c9a4ee725795194fb83e287114
ntorga <northontorga@gmail.com>
2023-11-14 16:49:43 +0000

    feat: add io and mp to storage unit info

--- src/domain/valueObject/storageUnitInfo.go
+++ src/domain/valueObject/storageUnitInfo.go
@@ -1,25 +1,39 @@
 package valueObject
 
 type StorageUnitInfo struct {
-	DeviceName     DeviceName `json:"deviceName"`
-	TotalBytes     Byte       `json:"totalBytes"`
-	AvailableBytes Byte       `json:"availableBytes"`
-	UsedBytes      Byte       `json:"usedBytes"`
-	UsedPercent    float64    `json:"usedPercent"`
+	DeviceName     DeviceName     `json:"deviceName"`
+	MountPoint     UnixFilePath   `json:"mountPoint"`
+	FileSystem     UnixFileSystem `json:"fileSystem"`
+	TotalBytes     Byte           `json:"totalBytes"`
+	AvailableBytes Byte           `json:"availableBytes"`
+	UsedBytes      Byte           `json:"usedBytes"`
+	UsedPercent    float64        `json:"usedPercent"`
+	ReadBytes      Byte           `json:"readBytes"`
+	ReadOpsCount   uint64         `json:"readOpsCount"`
+	WriteBytes     Byte           `json:"writeBytes"`
+	WriteOpsCount  uint64         `json:"writeOpsCount"`
 }
 
 func NewStorageUnitInfo(
 	deviceName DeviceName,
-	total Byte,
-	available Byte,
+	mountPoint UnixFilePath,
+	fileSystem UnixFileSystem,
+	totalBytes Byte,
+	availableBytes Byte,
 	usedBytes Byte,
 	usedPercent float64,
 ) StorageUnitInfo {
 	return StorageUnitInfo{
 		DeviceName:     deviceName,
-		TotalBytes:     total,
-		AvailableBytes: available,
+		MountPoint:     mountPoint,
+		FileSystem:     fileSystem,
+		TotalBytes:     totalBytes,
+		AvailableBytes: availableBytes,
 		UsedBytes:      usedBytes,
 		UsedPercent:    usedPercent,
+		ReadBytes:      0,
+		ReadOpsCount:   0,
+		WriteBytes:     0,
+		WriteOpsCount:  0,
 	}
 }
