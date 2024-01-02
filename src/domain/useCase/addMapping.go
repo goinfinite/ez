180991981f56f864b8cabe3d81b0b75adac5eb3e
ntorga <northontorga@gmail.com>
2024-01-02 20:24:07 +0000

    feat: handle duplicated mapping targets

--- src/domain/useCase/addMapping.go
+++ src/domain/useCase/addMapping.go
@@ -5,31 +5,95 @@ import (
 	"log"
 
 	"github.com/speedianet/control/src/domain/dto"
+	"github.com/speedianet/control/src/domain/entity"
 	"github.com/speedianet/control/src/domain/repository"
+	"github.com/speedianet/control/src/domain/valueObject"
 )
+
+func mappingTargetAlreadyExists(
+	mapping entity.Mapping,
+	target valueObject.MappingTarget,
+) bool {
+	if len(mapping.Targets) == 0 {
+		return false
+	}
+
+	for _, currentTarget := range mapping.Targets {
+		isSameContainerId := currentTarget.ContainerId == target.ContainerId
+		isSamePort := currentTarget.Port == target.Port
+		if isSameContainerId && isSamePort {
+			return true
+		}
+	}
+
+	return false
+}
+
+func addNewMappingTargets(
+	mappingCmdRepo repository.MappingCmdRepo,
+	existingMapping *entity.Mapping,
+	newTargets []valueObject.MappingTarget,
+) error {
+	targetsToAdd := []valueObject.MappingTarget{}
+	for _, newTarget := range newTargets {
+		if mappingTargetAlreadyExists(*existingMapping, newTarget) {
+			continue
+		}
+
+		targetsToAdd = append(targetsToAdd, newTarget)
+	}
+
+	if len(targetsToAdd) == 0 {
+		return errors.New("NoNewTargetsToAdd")
+	}
+
+	err := mappingCmdRepo.AddTargets(
+		existingMapping.Id,
+		targetsToAdd,
+	)
+	if err != nil {
+		log.Printf("AddMappingTargetsError: %s", err)
+		return errors.New("AddMappingTargetsInfraError")
+	}
+
+	return nil
+}
 
 func AddMapping(
 	mappingQueryRepo repository.MappingQueryRepo,
 	mappingCmdRepo repository.MappingCmdRepo,
 	addMapping dto.AddMapping,
 ) error {
-	_, err := mappingQueryRepo.GetByHostPortProtocol(
-		addMapping.Hostname,
-		addMapping.Port,
-		addMapping.Protocol,
-	)
-	if err == nil {
-		return errors.New("MappingAlreadyExists")
+	if len(addMapping.Targets) == 0 {
+		return errors.New("NoTargetsToAdd")
 	}
 
 	wasHostnameSent := addMapping.Hostname != nil
 
 	isTcp := addMapping.Protocol.String() == "tcp"
 	isUdp := addMapping.Protocol.String() == "udp"
-	isProtocolTransportLayer := isTcp || isUdp
+	isTransportLayer := isTcp || isUdp
 
-	if wasHostnameSent && isProtocolTransportLayer {
+	if wasHostnameSent && isTransportLayer {
 		return errors.New("TransportLayerCannotHaveHostname")
+	}
+
+	existingMapping, err := mappingQueryRepo.FindOne(
+		addMapping.Hostname,
+		addMapping.Port,
+		addMapping.Protocol,
+	)
+	if err != nil && err.Error() != "MappingNotFound" {
+		log.Printf("FindOneMappingError: %s", err)
+		return errors.New("FindOneMappingInfraError")
+	}
+
+	if existingMapping != nil {
+		return addNewMappingTargets(
+			mappingCmdRepo,
+			existingMapping,
+			addMapping.Targets,
+		)
 	}
 
 	err = mappingCmdRepo.Add(addMapping)
