3f372413574589635e4a7c0526b87ac6d46d3241
ntorga <northontorga@gmail.com>
2024-01-05 13:28:17 +0000

    fix: remove ipAddr and turn entrypoint a pointer

--- src/domain/entity/container.go
+++ src/domain/entity/container.go
@@ -3,21 +3,20 @@ package entity
 import "github.com/speedianet/control/src/domain/valueObject"
 
 type Container struct {
-	Id               valueObject.ContainerId            `json:"id"`
-	AccountId        valueObject.AccountId              `json:"accountId"`
-	Hostname         valueObject.Fqdn                   `json:"hostname"`
-	Status           bool                               `json:"status"`
-	Image            valueObject.ContainerImgAddress    `json:"image"`
-	ImageHash        valueObject.Hash                   `json:"imageHash"`
-	PrivateIpAddress valueObject.IpAddress              `json:"privateIp"`
-	PortBindings     []valueObject.PortBinding          `json:"portBindings"`
-	RestartPolicy    valueObject.ContainerRestartPolicy `json:"restartPolicy"`
-	RestartCount     uint64                             `json:"restartCount"`
-	Entrypoint       valueObject.ContainerEntrypoint    `json:"entrypoint"`
-	CreatedAt        valueObject.UnixTime               `json:"createdAt"`
-	StartedAt        *valueObject.UnixTime              `json:"startedAt"`
-	ProfileId        valueObject.ContainerProfileId     `json:"profileId"`
-	Envs             []valueObject.ContainerEnv         `json:"envs"`
+	Id            valueObject.ContainerId            `json:"id"`
+	AccountId     valueObject.AccountId              `json:"accountId"`
+	Hostname      valueObject.Fqdn                   `json:"hostname"`
+	Status        bool                               `json:"status"`
+	ImageAddr     valueObject.ContainerImgAddress    `json:"imageAddr"`
+	ImageHash     valueObject.Hash                   `json:"imageHash"`
+	PortBindings  []valueObject.PortBinding          `json:"portBindings"`
+	RestartPolicy valueObject.ContainerRestartPolicy `json:"restartPolicy"`
+	RestartCount  uint64                             `json:"restartCount"`
+	Entrypoint    *valueObject.ContainerEntrypoint   `json:"entrypoint"`
+	CreatedAt     valueObject.UnixTime               `json:"createdAt"`
+	StartedAt     *valueObject.UnixTime              `json:"startedAt"`
+	ProfileId     valueObject.ContainerProfileId     `json:"profileId"`
+	Envs          []valueObject.ContainerEnv         `json:"envs"`
 }
 
 func NewContainer(
@@ -25,33 +24,31 @@ func NewContainer(
 	accountId valueObject.AccountId,
 	hostname valueObject.Fqdn,
 	status bool,
-	image valueObject.ContainerImgAddress,
+	imageAddr valueObject.ContainerImgAddress,
 	imageHash valueObject.Hash,
-	privateIpAddress valueObject.IpAddress,
 	portBindings []valueObject.PortBinding,
 	restartPolicy valueObject.ContainerRestartPolicy,
 	restartCount uint64,
-	entrypoint valueObject.ContainerEntrypoint,
+	entrypoint *valueObject.ContainerEntrypoint,
 	createdAt valueObject.UnixTime,
 	startedAt *valueObject.UnixTime,
 	profileId valueObject.ContainerProfileId,
 	envs []valueObject.ContainerEnv,
 ) Container {
 	return Container{
-		Id:               id,
-		AccountId:        accountId,
-		Hostname:         hostname,
-		Status:           status,
-		Image:            image,
-		ImageHash:        imageHash,
-		PrivateIpAddress: privateIpAddress,
-		PortBindings:     portBindings,
-		RestartPolicy:    restartPolicy,
-		RestartCount:     restartCount,
-		Entrypoint:       entrypoint,
-		CreatedAt:        createdAt,
-		StartedAt:        startedAt,
-		ProfileId:        profileId,
-		Envs:             envs,
+		Id:            id,
+		AccountId:     accountId,
+		Hostname:      hostname,
+		Status:        status,
+		ImageAddr:     imageAddr,
+		ImageHash:     imageHash,
+		PortBindings:  portBindings,
+		RestartPolicy: restartPolicy,
+		RestartCount:  restartCount,
+		Entrypoint:    entrypoint,
+		CreatedAt:     createdAt,
+		StartedAt:     startedAt,
+		ProfileId:     profileId,
+		Envs:          envs,
 	}
 }
