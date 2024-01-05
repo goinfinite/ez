4efb8515143c1c13889ad7d7cb0cebb2833f179a
ntorga <northontorga@gmail.com>
2024-01-05 13:28:38 +0000

    feat: add private port to port binding

--- src/domain/valueObject/portBinding.go
+++ src/domain/valueObject/portBinding.go
@@ -1,31 +1,119 @@
 package valueObject
 
+import (
+	"errors"
+	"strings"
+)
+
 type PortBinding struct {
-	Protocol      NetworkProtocol `json:"protocol"`
-	HostPort      NetworkPort     `json:"hostPort"`
+	PublicPort    NetworkPort     `json:"publicPort"`
 	ContainerPort NetworkPort     `json:"containerPort"`
+	Protocol      NetworkProtocol `json:"protocol"`
+	PrivatePort   *NetworkPort    `json:"privatePort"`
 }
 
 func NewPortBinding(
-	protocol NetworkProtocol,
-	hostPort NetworkPort,
+	publicPort NetworkPort,
 	containerPort NetworkPort,
+	protocol NetworkProtocol,
+	privatePort *NetworkPort,
 ) PortBinding {
 	return PortBinding{
-		Protocol:      protocol,
-		HostPort:      hostPort,
+		PublicPort:    publicPort,
 		ContainerPort: containerPort,
+		Protocol:      protocol,
+		PrivatePort:   privatePort,
 	}
+}
+
+// format: <publicPort>:<containerPort>/<protocol>:<privatePort>
+func NewPortBindingFromString(value string) (PortBinding, error) {
+	var portBinding PortBinding
+
+	bindingParts := strings.Split(value, ":")
+	if len(bindingParts) == 0 {
+		return portBinding, errors.New("InvalidPortBinding")
+	}
+
+	publicPort, err := NewNetworkPort(bindingParts[0])
+	if err != nil {
+		return portBinding, err
+	}
+
+	defaultProtocol, _ := NewNetworkProtocol("tcp")
+
+	if len(bindingParts) == 1 {
+		return NewPortBinding(
+			publicPort,
+			publicPort,
+			defaultProtocol,
+			nil,
+		), nil
+	}
+
+	containerPortProtocolParts := strings.Split(bindingParts[1], "/")
+	containerPortStr := containerPortProtocolParts[0]
+	protocolStr := defaultProtocol.String()
+	if len(containerPortProtocolParts) == 2 {
+		protocolStr = containerPortProtocolParts[1]
+	}
+
+	containerPort, err := NewNetworkPort(containerPortStr)
+	if err != nil {
+		return portBinding, err
+	}
+
+	protocol, err := NewNetworkProtocol(protocolStr)
+	if err != nil {
+		return portBinding, err
+	}
+
+	if len(bindingParts) == 2 {
+		return NewPortBinding(
+			publicPort,
+			containerPort,
+			protocol,
+			nil,
+		), nil
+	}
+
+	privatePort, err := NewNetworkPort(bindingParts[2])
+	if err != nil {
+		return portBinding, err
+	}
+
+	return NewPortBinding(
+		publicPort,
+		containerPort,
+		protocol,
+		&privatePort,
+	), nil
+}
+
+func (portBinding PortBinding) GetPublicPort() NetworkPort {
+	return portBinding.PublicPort
+}
+
+func (portBinding PortBinding) GetContainerPort() NetworkPort {
+	return portBinding.ContainerPort
 }
 
 func (portBinding PortBinding) GetProtocol() NetworkProtocol {
 	return portBinding.Protocol
 }
 
-func (portBinding PortBinding) GetHostPort() NetworkPort {
-	return portBinding.HostPort
-}
+func (portBinding PortBinding) String() string {
+	containerPortWithProtocol := portBinding.ContainerPort.String() +
+		"/" + portBinding.Protocol.String()
 
-func (portBinding PortBinding) GetContainerPort() NetworkPort {
-	return portBinding.ContainerPort
+	stringParts := []string{
+		portBinding.PublicPort.String(),
+		containerPortWithProtocol,
+	}
+
+	if portBinding.PrivatePort != nil {
+		stringParts = append(stringParts, portBinding.PrivatePort.String())
+	}
+
+	return strings.Join(stringParts, ":")
 }
