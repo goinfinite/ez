16ccd69cc7271bf36f3f2fb8bf2584166f93bc32
ntorga <northontorga@gmail.com>
2024-01-13 20:52:38 +0000

    refactor: use db for containers query

--- src/infra/containerQueryRepo.go
+++ src/infra/containerQueryRepo.go
@@ -5,12 +5,12 @@ import (
 	"errors"
 	"log"
 	"strings"
-	"time"
 
 	"github.com/speedianet/control/src/domain/dto"
 	"github.com/speedianet/control/src/domain/entity"
 	"github.com/speedianet/control/src/domain/valueObject"
 	"github.com/speedianet/control/src/infra/db"
+	dbModel "github.com/speedianet/control/src/infra/db/model"
 	infraHelper "github.com/speedianet/control/src/infra/helper"
 )
 
@@ -22,297 +22,73 @@ func NewContainerQueryRepo(dbSvc *db.DatabaseService) *ContainerQueryRepo {
 	return &ContainerQueryRepo{dbSvc: dbSvc}
 }
 
-func (repo ContainerQueryRepo) parsePortBindings(
-	rawPortBindings map[string]interface{},
-) ([]valueObject.PortBinding, error) {
-	portBindings := []valueObject.PortBinding{}
-
-	for containerPortProtocol, hostIpHostPortsMap := range rawPortBindings {
-		containerPortProtocol := strings.Split(containerPortProtocol, "/")
-		if len(containerPortProtocol) != 2 {
-			return portBindings, errors.New("PortBindingInfoParseError")
-		}
-
-		containerPort, err := valueObject.NewNetworkPort(containerPortProtocol[0])
-		if err != nil {
-			return portBindings, err
-		}
-
-		networkProtocol, err := valueObject.NewNetworkProtocol(containerPortProtocol[1])
-		if err != nil {
-			return portBindings, err
-		}
-
-		for _, hostIpHostPort := range hostIpHostPortsMap.([]interface{}) {
-			hostIpHostPort, assertOk := hostIpHostPort.(map[string]interface{})
-			if !assertOk {
-				return portBindings, errors.New("PortBindingInfoParseError")
-			}
-
-			rawHostPort, assertOk := hostIpHostPort["HostPort"].(string)
-			if !assertOk {
-				return portBindings, errors.New("HostPortParseError")
-			}
-
-			privatePort, err := valueObject.NewNetworkPort(rawHostPort)
-			if err != nil {
-				return portBindings, err
-			}
-
-			portBinding := valueObject.NewPortBinding(
-				containerPort,
-				containerPort,
-				networkProtocol,
-				&privatePort,
-			)
-			portBindings = append(portBindings, portBinding)
-		}
-	}
-
-	return portBindings, nil
-}
-
 func (repo ContainerQueryRepo) GetById(
 	accId valueObject.AccountId,
 	containerId valueObject.ContainerId,
 ) (entity.Container, error) {
-	containerInfoJson, err := infraHelper.RunCmdAsUser(
-		accId,
-		"podman",
-		"container",
-		"inspect",
+	var containerModel dbModel.Container
+	err := repo.dbSvc.Orm.Where(
+		"account_id = ? AND id = ?",
+		accId.Get(),
 		containerId.String(),
-		"--format",
-		"{{json .}}",
-	)
+	).First(&containerModel).Error
 	if err != nil {
 		return entity.Container{}, err
 	}
 
-	containerInfo := map[string]interface{}{}
-	err = json.Unmarshal([]byte(containerInfoJson), &containerInfo)
+	containerEntity, err := containerModel.ToEntity()
 	if err != nil {
-		return entity.Container{}, err
+		return containerEntity, err
 	}
 
-	rawConfig, assertOk := containerInfo["Config"].(map[string]interface{})
-	if !assertOk {
-		return entity.Container{}, errors.New("PodmanConfigParseError")
-	}
-
-	rawHostname, assertOk := rawConfig["Hostname"].(string)
-	if !assertOk {
-		return entity.Container{}, errors.New("HostnameParseError")
-	}
-	hostname, err := valueObject.NewFqdn(rawHostname)
-	if err != nil {
-		return entity.Container{}, err
-	}
-
-	rawState, assertOk := containerInfo["State"].(map[string]interface{})
-	if !assertOk {
-		return entity.Container{}, errors.New("StateParseError")
-	}
-
-	status, assertOk := rawState["Running"].(bool)
-	if !assertOk {
-		return entity.Container{}, errors.New("StatusParseError")
-	}
-
-	rawImageAddr, assertOk := containerInfo["ImageName"].(string)
-	if !assertOk {
-		return entity.Container{}, errors.New("ImageParseError")
-	}
-	imageAddr, err := valueObject.NewContainerImgAddress(rawImageAddr)
-	if err != nil {
-		return entity.Container{}, err
-	}
-
-	rawImageHash, assertOk := containerInfo["ImageDigest"].(string)
-	if !assertOk {
-		return entity.Container{}, errors.New("ImageHashParseError")
-	}
-	rawImageHash = strings.TrimLeft(rawImageHash, "sha256:")
-	imageHash, err := valueObject.NewHash(rawImageHash)
-	if err != nil {
-		return entity.Container{}, err
-	}
-
-	rawHostConfig, assertOk := containerInfo["HostConfig"].(map[string]interface{})
-	if !assertOk {
-		return entity.Container{}, errors.New("HostConfigParseError")
-	}
-
-	rawPortBindings, assertOk := rawHostConfig["PortBindings"].(map[string]interface{})
-	if !assertOk {
-		return entity.Container{}, errors.New("PortBindingsParseError")
-	}
-	portBindings, err := repo.parsePortBindings(rawPortBindings)
-	if err != nil {
-		return entity.Container{}, err
-	}
-
-	rawRestartPolicy, assertOk := rawHostConfig["RestartPolicy"].(map[string]interface{})
-	if !assertOk {
-		return entity.Container{}, errors.New("RestartPolicyParseError")
-	}
-	rawRestartPolicyName := rawRestartPolicy["Name"].(string)
-	if rawRestartPolicyName == "" {
-		rawRestartPolicyName = "no"
-	}
-	restartPolicyName, err := valueObject.NewContainerRestartPolicy(rawRestartPolicyName)
-	if err != nil {
-		return entity.Container{}, err
-	}
-
-	rawRestartCount, assertOk := containerInfo["RestartCount"].(float64)
-	if !assertOk {
-		return entity.Container{}, errors.New("RestartCountParseError")
-	}
-	restartCount := uint64(rawRestartCount)
-
-	var entrypointPtr *valueObject.ContainerEntrypoint
-	rawEntryPoint, assertOk := rawConfig["Entrypoint"].(string)
-	if assertOk {
-		entrypoint, err := valueObject.NewContainerEntrypoint(rawEntryPoint)
-		if err != nil {
-			return entity.Container{}, err
-		}
-		entrypointPtr = &entrypoint
-	}
-
-	rawCreatedAt, assertOk := containerInfo["Created"].(string)
-	if !assertOk {
-		return entity.Container{}, errors.New("CreatedAtParseError")
-	}
-	createdAtTime, err := time.Parse(time.RFC3339, rawCreatedAt)
-	if err != nil {
-		return entity.Container{}, err
-	}
-	createdAt := valueObject.UnixTime(createdAtTime.Unix())
-
-	var startedAtPtr *valueObject.UnixTime
-	rawStartedAt, assertOk := rawState["StartedAt"].(string)
-	if !assertOk {
-		return entity.Container{}, errors.New("StartedAtParseError")
-	}
-	startedAtTime, err := time.Parse(time.RFC3339, rawStartedAt)
-	if err == nil {
-		startedAt := valueObject.UnixTime(startedAtTime.Unix())
-		startedAtPtr = &startedAt
-	}
-
-	containerName, assertOk := containerInfo["Name"].(string)
-	if !assertOk {
-		return entity.Container{}, errors.New("ContainerNameNotString")
-	}
-
-	containerNameParts := strings.Split(containerName, "-")
-	if len(containerNameParts) < 2 {
-		return entity.Container{}, errors.New("ContainerNameParseError")
-	}
-
-	profileId, err := valueObject.NewContainerProfileId(containerNameParts[0])
-	if err != nil {
-		return entity.Container{}, err
-	}
-
-	rawEnvs, assertOk := rawConfig["Env"].([]interface{})
-	if !assertOk {
-		return entity.Container{}, errors.New("EnvParseError")
-	}
-	envs := []valueObject.ContainerEnv{}
-	for _, rawEnv := range rawEnvs {
-		env, err := valueObject.NewContainerEnv(rawEnv.(string))
-		if err != nil {
-			continue
-		}
-		envs = append(envs, env)
-	}
-
-	return entity.NewContainer(
-		containerId,
-		accId,
-		hostname,
-		status,
-		imageAddr,
-		imageHash,
-		portBindings,
-		restartPolicyName,
-		restartCount,
-		entrypointPtr,
-		createdAt,
-		startedAtPtr,
-		profileId,
-		envs,
-	), nil
+	return containerEntity, nil
 }
 
 func (repo ContainerQueryRepo) GetByAccId(
 	accId valueObject.AccountId,
 ) ([]entity.Container, error) {
-	containersIds, err := infraHelper.RunCmdAsUser(
-		accId,
-		"podman",
-		"container",
-		"list",
-		"--all",
-		"--format",
-		"{{.ID}}",
-	)
-	if err != nil {
-		return []entity.Container{}, errors.New("AccPodmanListError" + err.Error())
-	}
-	containersIdsList := strings.Split(containersIds, "\n")
-	if len(containersIdsList) == 0 {
-		return []entity.Container{}, nil
-	}
-
 	containers := []entity.Container{}
-	for _, containerIdStr := range containersIdsList {
-		containerIdStr = strings.TrimSpace(containerIdStr)
-		containerId, err := valueObject.NewContainerId(containerIdStr)
-		if err != nil {
-			continue
-		}
 
-		container, err := repo.GetById(accId, containerId)
+	containerModels := []dbModel.Container{}
+	err := repo.dbSvc.Orm.Where(
+		"account_id = ?",
+		accId.Get(),
+	).Find(&containerModels).Error
+	if err != nil {
+		return containers, err
+	}
+
+	for _, containerModel := range containerModels {
+		containerEntity, err := containerModel.ToEntity()
 		if err != nil {
-			log.Printf(
-				"ContainerId '%s' skipped. ParseError: %s",
-				containerId.String(),
-				err.Error(),
-			)
+			log.Printf("[%s] %s", containerModel.ID, err.Error())
 			continue
 		}
-		containers = append(containers, container)
+		containers = append(containers, containerEntity)
 	}
 
 	return containers, nil
 }
 
 func (repo ContainerQueryRepo) Get() ([]entity.Container, error) {
-	allContainers := []entity.Container{}
+	containers := []entity.Container{}
 
-	accsList, err := NewAccQueryRepo(repo.dbSvc).Get()
+	containerModels := []dbModel.Container{}
+	err := repo.dbSvc.Orm.Find(&containerModels).Error
 	if err != nil {
-		return allContainers, err
+		return containers, err
 	}
 
-	for _, acc := range accsList {
-		accContainers, err := repo.GetByAccId(acc.Id)
+	for _, containerModel := range containerModels {
+		containerEntity, err := containerModel.ToEntity()
 		if err != nil {
-			log.Printf(
-				"AccId '%s' skipped. ParseError: %s",
-				acc.Id.String(),
-				err.Error(),
-			)
+			log.Printf("[%s] %s", containerModel.ID, err.Error())
 			continue
 		}
-		allContainers = append(allContainers, accContainers...)
+		containers = append(containers, containerEntity)
 	}
 
-	return allContainers, nil
+	return containers, nil
 }
 
 func (repo ContainerQueryRepo) containerMetricsFactory(
