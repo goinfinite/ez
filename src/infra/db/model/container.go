364acfdadc526f38836caa41f88758d818107278
ntorga <northontorga@gmail.com>
2024-01-13 18:17:58 +0000

    feat: add container and port bindings models

---
+++ src/infra/db/model/container.go
@@ -0,0 +1,180 @@
+package dbModel
+
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/speedianet/control/src/domain/entity"
+	"github.com/speedianet/control/src/domain/valueObject"
+)
+
+type Container struct {
+	ID            string `gorm:"primarykey"`
+	AccountID     uint   `gorm:"not null"`
+	Hostname      string `gorm:"not null"`
+	Status        bool   `gorm:"not null"`
+	ImageAddr     string `gorm:"not null"`
+	ImageHash     string `gorm:"not null"`
+	PortBindings  []ContainerPortBinding
+	RestartPolicy string `gorm:"not null"`
+	RestartCount  uint
+	Entrypoint    *string
+	CreatedAt     time.Time `gorm:"not null"`
+	StartedAt     *time.Time
+	ProfileID     uint `gorm:"not null"`
+	Envs          *string
+}
+
+func (Container) TableName() string {
+	return "containers"
+}
+
+func (model Container) ToEntity() (entity.Container, error) {
+	var containerEntity entity.Container
+
+	id, err := valueObject.NewContainerId(model.ID)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	accountId, err := valueObject.NewAccountId(model.AccountID)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	hostname, err := valueObject.NewFqdn(model.Hostname)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	imageAddr, err := valueObject.NewContainerImgAddress(model.ImageAddr)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	imageHash, err := valueObject.NewHash(model.ImageHash)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	var portBindings []valueObject.PortBinding
+	if len(model.PortBindings) > 0 {
+		for _, portBindingModel := range model.PortBindings {
+			portBinding, err := portBindingModel.ToValueObject()
+			if err != nil {
+				return containerEntity, err
+			}
+			portBindings = append(portBindings, portBinding)
+		}
+	}
+
+	restartPolicy, err := valueObject.NewContainerRestartPolicy(model.RestartPolicy)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	var entryPointPtr *valueObject.ContainerEntrypoint
+	if model.Entrypoint != nil {
+		entryPoint, err := valueObject.NewContainerEntrypoint(*model.Entrypoint)
+		if err != nil {
+			return containerEntity, err
+		}
+		entryPointPtr = &entryPoint
+	}
+
+	createdAt := valueObject.UnixTime(model.CreatedAt.Unix())
+
+	var startedAtPtr *valueObject.UnixTime
+	if model.StartedAt != nil {
+		startedAt := valueObject.UnixTime(model.StartedAt.Unix())
+		startedAtPtr = &startedAt
+	}
+
+	profileId, err := valueObject.NewContainerProfileId(model.ProfileID)
+	if err != nil {
+		return containerEntity, err
+	}
+
+	envs := []valueObject.ContainerEnv{}
+	if model.Envs != nil {
+		envsParts := strings.Split(*model.Envs, ";")
+		if len(envsParts) == 0 {
+			return containerEntity, errors.New("InvalidEnvs")
+		}
+
+		for _, envPart := range envsParts {
+			env, err := valueObject.NewContainerEnv(envPart)
+			if err != nil {
+				return containerEntity, err
+			}
+			envs = append(envs, env)
+		}
+	}
+
+	return entity.NewContainer(
+		id,
+		accountId,
+		hostname,
+		model.Status,
+		imageAddr,
+		imageHash,
+		portBindings,
+		restartPolicy,
+		uint64(model.RestartCount),
+		entryPointPtr,
+		createdAt,
+		startedAtPtr,
+		profileId,
+		envs,
+	), nil
+}
+
+func (Container) ToModel(entity entity.Container) Container {
+	portBindingModels := []ContainerPortBinding{}
+	for _, portBinding := range entity.PortBindings {
+		portBindingModel := ContainerPortBinding{}.ToModel(
+			entity.Id,
+			portBinding,
+		)
+		portBindingModels = append(portBindingModels, portBindingModel)
+	}
+
+	var entrypointStrPtr *string
+	if entity.Entrypoint != nil {
+		entrypointStr := entity.Entrypoint.String()
+		entrypointStrPtr = &entrypointStr
+	}
+
+	var startedAtPtr *time.Time
+	if entity.StartedAt != nil {
+		startedAt := time.Unix(entity.StartedAt.Get(), 0)
+		startedAtPtr = &startedAt
+	}
+
+	var envsPtr *string
+	if len(entity.Envs) > 0 {
+		envs := ""
+		for _, env := range entity.Envs {
+			envs += env.String() + ";"
+		}
+		envsPtr = &envs
+	}
+
+	return Container{
+		ID:            entity.Id.String(),
+		AccountID:     uint(entity.AccountId.Get()),
+		Hostname:      entity.Hostname.String(),
+		Status:        entity.Status,
+		ImageAddr:     entity.ImageAddr.String(),
+		ImageHash:     entity.ImageHash.String(),
+		PortBindings:  portBindingModels,
+		RestartPolicy: entity.RestartPolicy.String(),
+		RestartCount:  uint(entity.RestartCount),
+		Entrypoint:    entrypointStrPtr,
+		CreatedAt:     time.Unix(entity.CreatedAt.Get(), 0),
+		StartedAt:     startedAtPtr,
+		ProfileID:     uint(entity.ProfileId.Get()),
+		Envs:          envsPtr,
+	}
+}
---
+++ src/infra/db/model/containerPortBinding.go
@@ -0,0 +1,104 @@
+package dbModel
+
+import (
+	"errors"
+
+	"github.com/speedianet/control/src/domain/valueObject"
+	"gorm.io/gorm"
+)
+
+type ContainerPortBinding struct {
+	ID            uint   `gorm:"primarykey"`
+	ContainerID   string `gorm:"not null"`
+	PublicPort    uint   `gorm:"not null"`
+	ContainerPort uint   `gorm:"not null"`
+	Protocol      string `gorm:"not null"`
+	PrivatePort   uint   `gorm:"not null"`
+}
+
+func (ContainerPortBinding) TableName() string {
+	return "container_port_bindings"
+}
+
+func (ContainerPortBinding) ToModel(
+	containerId valueObject.ContainerId,
+	vo valueObject.PortBinding,
+) ContainerPortBinding {
+	return ContainerPortBinding{
+		ContainerID:   containerId.String(),
+		PublicPort:    uint(vo.PublicPort.Get()),
+		ContainerPort: uint(vo.ContainerPort.Get()),
+		Protocol:      vo.Protocol.String(),
+		PrivatePort:   uint(vo.PrivatePort.Get()),
+	}
+}
+
+func (model ContainerPortBinding) ToValueObject() (valueObject.PortBinding, error) {
+	var portBinding valueObject.PortBinding
+
+	publicPort, err := valueObject.NewNetworkPort(model.PublicPort)
+	if err != nil {
+		return portBinding, err
+	}
+
+	containerPort, err := valueObject.NewNetworkPort(model.ContainerPort)
+	if err != nil {
+		return portBinding, err
+	}
+
+	protocol, err := valueObject.NewNetworkProtocol(model.Protocol)
+	if err != nil {
+		return portBinding, err
+	}
+
+	privatePort, err := valueObject.NewNetworkPort(model.PrivatePort)
+	if err != nil {
+		return portBinding, err
+	}
+
+	return valueObject.NewPortBinding(
+		publicPort,
+		containerPort,
+		protocol,
+		&privatePort,
+	), nil
+}
+
+func (model ContainerPortBinding) GetNextAvailablePrivatePort(
+	ormSvc *gorm.DB,
+	portsToIgnore []valueObject.NetworkPort,
+) (valueObject.NetworkPort, error) {
+	usedPrivatePorts := []uint{}
+
+	err := ormSvc.Model(model).
+		Select("private_port").
+		Order("private_port asc").
+		Find(&usedPrivatePorts).Error
+	if err != nil {
+		return 0, err
+	}
+
+	if len(usedPrivatePorts) > 0 {
+		portsToIgnoreUint := []uint{}
+		for _, port := range portsToIgnore {
+			portsToIgnoreUint = append(portsToIgnoreUint, uint(port.Get()))
+		}
+		usedPrivatePorts = append(usedPrivatePorts, portsToIgnoreUint...)
+	}
+
+	initialPort := uint(40000)
+
+	nextPort := initialPort
+	for _, port := range usedPrivatePorts {
+		if port != nextPort {
+			model.PrivatePort = nextPort
+		}
+		nextPort++
+	}
+
+	if nextPort > 60000 {
+		return 0, errors.New("NoAvailablePrivatePort")
+	}
+
+	return valueObject.NewNetworkPort(model.PrivatePort)
+}
