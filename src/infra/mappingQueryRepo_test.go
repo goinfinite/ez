ac239e6ccaefcd9ccaeff3c31d844b075c8eb60a
ntorga <northontorga@gmail.com>
2024-01-17 12:40:40 +0000

    feat: add mapping query repo unit test

---
+++ src/infra/mappingQueryRepo_test.go
@@ -0,0 +1,105 @@
+package infra
+
+import (
+	"testing"
+
+	testHelpers "github.com/speedianet/control/src/devUtils"
+	"github.com/speedianet/control/src/domain/valueObject"
+)
+
+func TestMappingQueryRepo(t *testing.T) {
+	testHelpers.LoadEnvVars()
+	dbSvc := testHelpers.GetDbSvc()
+	mappingQueryRepo := NewMappingQueryRepo(dbSvc)
+
+	t.Run("GetMappings", func(t *testing.T) {
+		mappingList, err := mappingQueryRepo.Get()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if len(mappingList) == 0 {
+			t.Error("NoMappingsFound")
+			return
+		}
+	})
+
+	t.Run("GetMappingById", func(t *testing.T) {
+		mapping, err := mappingQueryRepo.GetById(1)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if mapping.Id.Get() != 1 {
+			t.Error("MappingNotFound")
+			return
+		}
+	})
+
+	t.Run("GetTargetById", func(t *testing.T) {
+		_, err := mappingQueryRepo.GetTargetById(1)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	})
+
+	t.Run("FindOneMappingWithoutHostname", func(t *testing.T) {
+		_, err := mappingQueryRepo.FindOne(
+			nil,
+			valueObject.NewNetworkPortPanic(80),
+			valueObject.NewNetworkProtocolPanic("http"),
+		)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	})
+
+	t.Run("FindOneMappingWithHostname", func(t *testing.T) {
+		hostname := valueObject.NewFqdnPanic("speedia.net")
+
+		_, err := mappingQueryRepo.FindOne(
+			&hostname,
+			valueObject.NewNetworkPortPanic(80),
+			valueObject.NewNetworkProtocolPanic("http"),
+		)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	})
+
+	t.Run("FindAllMappingWithoutHostname", func(t *testing.T) {
+		publicPort := valueObject.NewNetworkPortPanic(80)
+		protocol := valueObject.NewNetworkProtocolPanic("http")
+
+		_, err := mappingQueryRepo.FindAll(
+			nil,
+			&publicPort,
+			&protocol,
+		)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	})
+
+	t.Run("FindAllMappingWithHostname", func(t *testing.T) {
+		hostname := valueObject.NewFqdnPanic("speedia.net")
+		publicPort := valueObject.NewNetworkPortPanic(80)
+		protocol := valueObject.NewNetworkProtocolPanic("http")
+
+		_, err := mappingQueryRepo.FindAll(
+			&hostname,
+			&publicPort,
+			&protocol,
+		)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	})
+}
