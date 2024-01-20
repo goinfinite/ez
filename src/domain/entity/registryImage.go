375f343e335e2f1d3d254250947dc2022a3f4ac0
ntorga <northontorga@gmail.com>
2024-01-20 20:25:11 +0000

    feat: add constructor and turn description optional

--- src/domain/entity/registryImage.go
+++ src/domain/entity/registryImage.go
@@ -4,15 +4,42 @@ import "github.com/speedianet/control/src/domain/valueObject"
 
 type RegistryImage struct {
 	Name          valueObject.RegistryImageName            `json:"name"`
-	Description   valueObject.RegistryImageDescription     `json:"description"`
+	Description   *valueObject.RegistryImageDescription    `json:"description"`
 	RegistryName  valueObject.RegistryName                 `json:"registryName"`
 	PublisherName valueObject.RegistryPublisherName        `json:"publisherName"`
 	ImageAddress  valueObject.ContainerImageAddress        `json:"imageAddress"`
-	Isa           []valueObject.InstructionSetArchitecture `json:"isa"`
-	IsVerified    bool                                     `json:"isVerified"`
+	Isas          []valueObject.InstructionSetArchitecture `json:"isas"`
 	PullCount     uint64                                   `json:"pullCount"`
 	StarCount     *uint64                                  `json:"starCount"`
 	LogoUrl       *valueObject.Url                         `json:"logoUrl"`
 	CreatedAt     *valueObject.UnixTime                    `json:"createdAt"`
 	UpdatedAt     *valueObject.UnixTime                    `json:"updatedAt"`
+}
+
+func NewRegistryImage(
+	name valueObject.RegistryImageName,
+	description *valueObject.RegistryImageDescription,
+	registryName valueObject.RegistryName,
+	publisherName valueObject.RegistryPublisherName,
+	imageAddress valueObject.ContainerImageAddress,
+	isas []valueObject.InstructionSetArchitecture,
+	pullCount uint64,
+	starCount *uint64,
+	logoUrl *valueObject.Url,
+	createdAt *valueObject.UnixTime,
+	updatedAt *valueObject.UnixTime,
+) RegistryImage {
+	return RegistryImage{
+		Name:          name,
+		Description:   description,
+		RegistryName:  registryName,
+		PublisherName: publisherName,
+		ImageAddress:  imageAddress,
+		Isas:          isas,
+		PullCount:     pullCount,
+		StarCount:     starCount,
+		LogoUrl:       logoUrl,
+		CreatedAt:     createdAt,
+		UpdatedAt:     updatedAt,
+	}
 }
