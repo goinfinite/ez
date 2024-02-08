fcaa03391dbdbefe1735e7b702a4daba37d6832c
ntorga <northontorga@gmail.com>
2024-02-08 21:47:47 +0000

    fix: use acc controller on acc routes

--- src/presentation/cli/router.go
+++ src/presentation/cli/router.go
@@ -3,41 +3,35 @@ package cli
 import (
 	"fmt"
 
+	"github.com/speedianet/control/src/infra/db"
 	api "github.com/speedianet/control/src/presentation/api"
 	cliController "github.com/speedianet/control/src/presentation/cli/controller"
 	"github.com/spf13/cobra"
 )
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print software version",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Speedia Control v0.0.1")
-	},
+type CliRouter struct {
+	dbSvc *db.DatabaseService
 }
 
-var serveCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "Start the CONTROL server (default to port 10001)",
-	Run: func(cmd *cobra.Command, args []string) {
-		api.ApiInit()
-	},
+func NewCliRouter(dbSvc *db.DatabaseService) CliRouter {
+	return CliRouter{dbSvc: dbSvc}
 }
 
-func accountRoutes() {
+func (router CliRouter) accountRoutes() {
 	var accountCmd = &cobra.Command{
 		Use:   "account",
 		Short: "AccountManagement",
 	}
 
+	accountController := cliController.NewAccountController(router.dbSvc)
+	accountCmd.AddCommand(accountController.GetAccounts())
+	accountCmd.AddCommand(accountController.AddAccount())
+	accountCmd.AddCommand(accountController.UpdateAccount())
+	accountCmd.AddCommand(accountController.DeleteAccount())
 	rootCmd.AddCommand(accountCmd)
-	accountCmd.AddCommand(cliController.GetAccountsController())
-	accountCmd.AddCommand(cliController.AddAccountController())
-	accountCmd.AddCommand(cliController.UpdateAccountController())
-	accountCmd.AddCommand(cliController.DeleteAccountController())
 }
 
-func containerRoutes() {
+func (router CliRouter) containerRoutes() {
 	var containerCmd = &cobra.Command{
 		Use:   "container",
 		Short: "ContainerManagement",
@@ -70,7 +64,7 @@ func containerRoutes() {
 	containerRegistryCmd.AddCommand(cliController.GetRegistryTaggedImageController())
 }
 
-func licenseRoutes() {
+func (router CliRouter) licenseRoutes() {
 	var licenseCmd = &cobra.Command{
 		Use:   "license",
 		Short: "LicenseManagement",
@@ -80,7 +74,7 @@ func licenseRoutes() {
 	licenseCmd.AddCommand(cliController.GetLicenseInfoController())
 }
 
-func mappingRoutes() {
+func (router CliRouter) mappingRoutes() {
 	var mappingCmd = &cobra.Command{
 		Use:   "mapping",
 		Short: "MappingManagement",
@@ -101,7 +95,7 @@ func mappingRoutes() {
 	mappingTargetCmd.AddCommand(cliController.DeleteMappingTargetController())
 }
 
-func o11yRoutes() {
+func (router CliRouter) o11yRoutes() {
 	var o11yCmd = &cobra.Command{
 		Use:   "o11y",
 		Short: "O11yManagement",
@@ -111,13 +105,33 @@ func o11yRoutes() {
 	o11yCmd.AddCommand(cliController.GetO11yOverviewController())
 }
 
-func registerCliRoutes() {
+func (router CliRouter) systemRoutes() {
+	var versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print software version",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Speedia Control v0.0.1")
+		},
+	}
+
+	var serveCmd = &cobra.Command{
+		Use:   "serve",
+		Short: "Start the CONTROL server (default to port 3141)",
+		Run: func(cmd *cobra.Command, args []string) {
+			api.ApiInit()
+		},
+	}
+
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(serveCmd)
 	rootCmd.AddCommand(cliController.SysInstallController())
-	accountRoutes()
-	containerRoutes()
-	licenseRoutes()
-	mappingRoutes()
-	o11yRoutes()
+}
+
+func (router CliRouter) RegisterRoutes() {
+	router.accountRoutes()
+	router.containerRoutes()
+	router.licenseRoutes()
+	router.mappingRoutes()
+	router.o11yRoutes()
+	router.systemRoutes()
 }
