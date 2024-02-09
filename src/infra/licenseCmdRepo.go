51ec8e1a004831ed9b6289d729b75a4b00831637
ntorga <northontorga@gmail.com>
2024-02-09 18:00:32 +0000

    fix: move get license info via api to refresh status on cmd repo

--- src/infra/licenseCmdRepo.go
+++ src/infra/licenseCmdRepo.go
@@ -1,8 +1,17 @@
 package infra
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/speedianet/control/src/domain/entity"
 	"github.com/speedianet/control/src/domain/valueObject"
 	"github.com/speedianet/control/src/infra/db"
+	dbModel "github.com/speedianet/control/src/infra/db/model"
 )
 
 type LicenseCmdRepo struct {
@@ -14,7 +23,76 @@ func NewLicenseCmdRepo(dbSvc *db.DatabaseService) LicenseCmdRepo {
 }
 
 func (repo LicenseCmdRepo) RefreshStatus() error {
-	return nil
+	speediaApiUrl := "https://app.speedia.net/api/v1"
+	apiEndpoint := "/store/product/license/verify/1/"
+
+	licenseMethod, _ := valueObject.NewLicenseMethod("ip")
+
+	keyStr := os.Getenv("LICENSE_KEY")
+	if keyStr != "" {
+		licenseMethod, _ = valueObject.NewLicenseMethod("key")
+		apiEndpoint += "?key=" + keyStr
+	}
+
+	httpResponse, err := http.Get(speediaApiUrl + apiEndpoint)
+	if err != nil {
+		return errors.New("GetLicenseInfoFailed")
+	}
+	defer httpResponse.Body.Close()
+
+	if httpResponse.StatusCode != 200 {
+		return errors.New("LicenseInfoBadStatusCode")
+	}
+
+	responseBody, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return errors.New("ReadLicenseInfoFailed")
+	}
+
+	var parsedResponse map[string]interface{}
+	err = json.Unmarshal(responseBody, &parsedResponse)
+	if err != nil {
+		return errors.New("ParseLicenseInfoFailed")
+	}
+
+	parsedBody, assertOk := parsedResponse["body"].(map[string]interface{})
+	if !assertOk {
+		return errors.New("ParseLicenseBodyFailed")
+	}
+
+	rawStatus, assertOk := parsedBody["status"].(string)
+	if !assertOk {
+		return errors.New("ParseLicenseStatusFailed")
+	}
+	licenseStatus, err := valueObject.NewLicenseStatus(rawStatus)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Implement license fingerprint
+	licenseFingerprint, _ := valueObject.NewLicenseFingerprint("fingerprint")
+
+	rawExpiresAt, assertOk := parsedBody["expiresAt"].(float64)
+	if !assertOk {
+		return errors.New("ParseLicenseExpiresAtFailed")
+	}
+	expiresAt := valueObject.UnixTime(int64(rawExpiresAt))
+
+	todayDate := time.Now()
+	lastCheckAt := valueObject.UnixTime(todayDate.Unix())
+	updatedAt := lastCheckAt
+
+	licenseInfoEntity := entity.NewLicenseInfo(
+		licenseMethod,
+		licenseStatus,
+		licenseFingerprint,
+		expiresAt,
+		lastCheckAt,
+		updatedAt,
+	)
+
+	licenseInfoModel := dbModel.LicenseInfo{}.ToModel(licenseInfoEntity)
+	return repo.dbSvc.Orm.Save(&licenseInfoModel).Error
 }
 
 func (repo LicenseCmdRepo) UpdateStatus(valueObject.LicenseStatus) error {
--- src/infra/licenseQueryRepo.go
+++ src/infra/licenseQueryRepo.go
@@ -1,15 +1,7 @@
 package infra
 
 import (
-	"encoding/json"
-	"errors"
-	"io"
-	"net/http"
-	"os"
-	"time"
-
 	"github.com/speedianet/control/src/domain/entity"
-	"github.com/speedianet/control/src/domain/valueObject"
 	"github.com/speedianet/control/src/infra/db"
 )
 
@@ -22,73 +14,7 @@ func NewLicenseQueryRepo(dbSvc *db.DatabaseService) LicenseQueryRepo {
 }
 
 func (repo LicenseQueryRepo) Get() (entity.LicenseInfo, error) {
-	var licenseInfo entity.LicenseInfo
-
-	speediaApiUrl := "https://app.speedia.net/api/v1"
-	apiEndpoint := "/store/product/license/verify/1/"
-
-	licenseMethod, _ := valueObject.NewLicenseMethod("ip")
-
-	keyStr := os.Getenv("LICENSE_KEY")
-	if keyStr != "" {
-		licenseMethod, _ = valueObject.NewLicenseMethod("key")
-		apiEndpoint += "?key=" + keyStr
-	}
-
-	httpResponse, err := http.Get(speediaApiUrl + apiEndpoint)
-	if err != nil {
-		return licenseInfo, errors.New("GetLicenseInfoFailed")
-	}
-	defer httpResponse.Body.Close()
-
-	if httpResponse.StatusCode != 200 {
-		return licenseInfo, errors.New("LicenseInfoBadStatusCode")
-	}
-
-	responseBody, err := io.ReadAll(httpResponse.Body)
-	if err != nil {
-		return licenseInfo, errors.New("ReadLicenseInfoFailed")
-	}
-
-	var parsedResponse map[string]interface{}
-	err = json.Unmarshal(responseBody, &parsedResponse)
-	if err != nil {
-		return licenseInfo, errors.New("ParseLicenseInfoFailed")
-	}
-
-	parsedBody, assertOk := parsedResponse["body"].(map[string]interface{})
-	if !assertOk {
-		return licenseInfo, errors.New("ParseLicenseBodyFailed")
-	}
-
-	rawStatus, assertOk := parsedBody["status"].(string)
-	if !assertOk {
-		return licenseInfo, errors.New("ParseLicenseStatusFailed")
-	}
-	licenseStatus, err := valueObject.NewLicenseStatus(rawStatus)
-	if err != nil {
-		return licenseInfo, err
-	}
-
-	// TODO: Implement license fingerprint
-	licenseFingerprint, _ := valueObject.NewLicenseFingerprint("fingerprint")
-
-	rawExpiresAt, assertOk := parsedBody["expiresAt"].(float64)
-	if !assertOk {
-		return licenseInfo, errors.New("ParseLicenseExpiresAtFailed")
-	}
-	expiresAt := valueObject.UnixTime(int64(rawExpiresAt))
-
-	todayDate := time.Now()
-	lastCheckAt := valueObject.UnixTime(todayDate.Unix())
-
-	return entity.NewLicenseInfo(
-		licenseMethod,
-		licenseStatus,
-		licenseFingerprint,
-		expiresAt,
-		lastCheckAt,
-	), nil
+	return entity.LicenseInfo{}, nil
 }
 
 func (repo LicenseQueryRepo) GetErrorCount() (int, error) {
