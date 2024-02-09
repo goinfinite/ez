565af48928ecd88ed4ecb1589c9726dfe1dfc0af
ntorga <northontorga@gmail.com>
2024-02-09 17:46:08 +0000

    feat: impl get via api for now

--- src/infra/licenseQueryRepo.go
+++ src/infra/licenseQueryRepo.go
@@ -1,6 +1,11 @@
 package infra
 
 import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/speedianet/control/src/domain/entity"
@@ -17,12 +22,64 @@ func NewLicenseQueryRepo(dbSvc *db.DatabaseService) LicenseQueryRepo {
 }
 
 func (repo LicenseQueryRepo) Get() (entity.LicenseInfo, error) {
+	var licenseInfo entity.LicenseInfo
+
+	speediaApiUrl := "https://app.speedia.net/api/v1"
+	apiEndpoint := "/store/product/license/verify/1/"
+
 	licenseMethod, _ := valueObject.NewLicenseMethod("ip")
-	licenseStatus, _ := valueObject.NewLicenseStatus("active")
+
+	keyStr := os.Getenv("LICENSE_KEY")
+	if keyStr != "" {
+		licenseMethod, _ = valueObject.NewLicenseMethod("key")
+		apiEndpoint += "?key=" + keyStr
+	}
+
+	httpResponse, err := http.Get(speediaApiUrl + apiEndpoint)
+	if err != nil {
+		return licenseInfo, errors.New("GetLicenseInfoFailed")
+	}
+	defer httpResponse.Body.Close()
+
+	if httpResponse.StatusCode != 200 {
+		return licenseInfo, errors.New("LicenseInfoBadStatusCode")
+	}
+
+	responseBody, err := io.ReadAll(httpResponse.Body)
+	if err != nil {
+		return licenseInfo, errors.New("ReadLicenseInfoFailed")
+	}
+
+	var parsedResponse map[string]interface{}
+	err = json.Unmarshal(responseBody, &parsedResponse)
+	if err != nil {
+		return licenseInfo, errors.New("ParseLicenseInfoFailed")
+	}
+
+	parsedBody, assertOk := parsedResponse["body"].(map[string]interface{})
+	if !assertOk {
+		return licenseInfo, errors.New("ParseLicenseBodyFailed")
+	}
+
+	rawStatus, assertOk := parsedBody["status"].(string)
+	if !assertOk {
+		return licenseInfo, errors.New("ParseLicenseStatusFailed")
+	}
+	licenseStatus, err := valueObject.NewLicenseStatus(rawStatus)
+	if err != nil {
+		return licenseInfo, err
+	}
+
+	// TODO: Implement license fingerprint
 	licenseFingerprint, _ := valueObject.NewLicenseFingerprint("fingerprint")
 
+	rawExpiresAt, assertOk := parsedBody["expiresAt"].(float64)
+	if !assertOk {
+		return licenseInfo, errors.New("ParseLicenseExpiresAtFailed")
+	}
+	expiresAt := valueObject.UnixTime(int64(rawExpiresAt))
+
 	todayDate := time.Now()
-	expiresAt := valueObject.UnixTime(todayDate.AddDate(1, 0, 0).Unix())
 	lastCheckAt := valueObject.UnixTime(todayDate.Unix())
 
 	return entity.NewLicenseInfo(
