4341d0ed4949c855143c62d2d526561609be9041
ntorga <northontorga@gmail.com>
2024-04-05 21:11:15 +0000

    feat: set default values when max specs is set

--- src/domain/useCase/addContainerProfile.go
+++ src/domain/useCase/addContainerProfile.go
@@ -6,19 +6,47 @@ import (
 
 	"github.com/speedianet/control/src/domain/dto"
 	"github.com/speedianet/control/src/domain/repository"
+	"github.com/speedianet/control/src/domain/valueObject"
 )
 
 func AddContainerProfile(
 	containerProfileCmdRepo repository.ContainerProfileCmdRepo,
-	addContainerProfile dto.AddContainerProfile,
+	dto dto.AddContainerProfile,
 ) error {
-	err := containerProfileCmdRepo.Add(addContainerProfile)
+	if dto.MaxSpecs != nil {
+		if dto.ScalingPolicy == nil {
+			defaultPolicy := valueObject.DefaultScalingPolicy()
+			dto.ScalingPolicy = &defaultPolicy
+		}
+
+		if dto.ScalingThreshold == nil {
+			defaultThreshold := uint64(80)
+			dto.ScalingThreshold = &defaultThreshold
+		}
+
+		if dto.ScalingMaxDurationSecs == nil {
+			defaultMaxDuration := uint64(3600)
+			dto.ScalingMaxDurationSecs = &defaultMaxDuration
+		}
+
+		if dto.ScalingIntervalSecs == nil {
+			defaultInterval := uint64(180)
+			dto.ScalingIntervalSecs = &defaultInterval
+		}
+
+		if dto.HostMinCapacityPercent == nil {
+			defaultHostMinCapacity := valueObject.DefaultHostMinCapacity()
+			dto.HostMinCapacityPercent = &defaultHostMinCapacity
+		}
+	}
+
+	err := containerProfileCmdRepo.Add(dto)
 	if err != nil {
-		log.Printf("AddContainerProfileError: %s", err)
+		log.Printf("AddContainerProfileError: %v", err)
 		return errors.New("AddContainerProfileInfraError")
 	}
 
-	log.Printf("ContainerProfile '%v' added.", addContainerProfile.Name.String())
+	log.Printf("ContainerProfile '%s' added.", dto.Name.String())
 
 	return nil
 }
