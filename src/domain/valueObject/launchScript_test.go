550b40d37d48f245a220b1cd066b2c0b04efe7c3
ntorga <northontorga@gmail.com>
2024-04-29 02:56:44 +0000

    test: add valid encoded launch script

--- src/domain/valueObject/launchScript_test.go
+++ src/domain/valueObject/launchScript_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestNewLaunchScript(t *testing.T) {
-	t.Run("ValidLaunchScriptFromString", func(t *testing.T) {
+	t.Run("ValidLaunchScript", func(t *testing.T) {
 		validLaunchScripts := []string{
 			`echo "Hello, World!"`,
 			`echo "Hello, World!" > /tmp/hello.txt`,
@@ -26,7 +26,27 @@ func TestNewLaunchScript(t *testing.T) {
 		}
 	})
 
-	t.Run("InvalidLaunchScriptFromString", func(t *testing.T) {
+	t.Run("ValidEncodedLaunchScript", func(t *testing.T) {
+		validEncodedLaunchScripts := []string{
+			`ZWNobyAiSGVsbG8sIFdvcmxkISI=`,
+			`cHJpbnRlbnYgPiAvdG1wL2hlbGxvLnR4dA==`,
+			`VEVYVD0iSGVsbG8sIFdvcmxkISIKZWNobyAiJHtURVhUfSIgPiAvdG1wL2hlbGxvLnR4dDsKVEVYVD0iR29vZGJ5ZSwgV29ybGQhIgplY2hvICIke1RFWFR9IiA+IC90bXAvZ29vZGJ5ZS50eHQ=`,
+		}
+
+		for _, script := range validEncodedLaunchScripts {
+			encodedScript, err := NewEncodedContent(script)
+			if err != nil {
+				t.Errorf("ExpectingNoErrorButGot: %s [%s]", err.Error(), script)
+			}
+
+			_, err = NewLaunchScriptFromEncodedContent(encodedScript)
+			if err != nil {
+				t.Errorf("ExpectingNoErrorButGot: %s [%s]", err.Error(), script)
+			}
+		}
+	})
+
+	t.Run("InvalidLaunchScript", func(t *testing.T) {
 		invalidLaunchScripts := []string{
 			"",
 			"ls",
