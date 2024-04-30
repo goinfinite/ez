a999e79113931058013cedc2258dba66e8d0c53e
ntorga <northontorga@gmail.com>
2024-04-30 02:04:58 +0000

    feat: add mappings janitor

--- src/domain/useCase/deleteContainer.go
+++ src/domain/useCase/deleteContainer.go
@@ -4,9 +4,57 @@ import (
 	"errors"
 	"log"
 
+	"github.com/speedianet/control/src/domain/entity"
 	"github.com/speedianet/control/src/domain/repository"
 	"github.com/speedianet/control/src/domain/valueObject"
 )
+
+func mappingsJanitor(
+	mappingQueryRepo repository.MappingQueryRepo,
+	mappingCmdRepo repository.MappingCmdRepo,
+	containerEntity entity.Container,
+) error {
+	targets, err := mappingQueryRepo.GetTargetsByContainerId(containerEntity.Id)
+	if err != nil {
+		log.Printf("[%v] GetTargetsByContainerIdError: %s", containerEntity.Id, err)
+		return nil
+	}
+
+	for _, target := range targets {
+		err = mappingCmdRepo.DeleteTarget(target.Id)
+		if err != nil {
+			log.Printf("[%v] DeleteTargetError: %s", target.Id, err)
+			continue
+		}
+
+		log.Printf("TargetId '%v' deleted.", target.Id)
+	}
+
+	mappings, err := mappingQueryRepo.GetByHostname(containerEntity.Hostname)
+	if err != nil {
+		return nil
+	}
+
+	if len(mappings) == 0 {
+		return nil
+	}
+
+	for _, mapping := range mappings {
+		if len(mapping.Targets) != 0 {
+			continue
+		}
+
+		err = mappingCmdRepo.Delete(mapping.Id)
+		if err != nil {
+			log.Printf("[%v] DeleteMappingError: %s", mapping.Id, err)
+			continue
+		}
+
+		log.Printf("MappingId '%v' deleted.", mapping.Id)
+	}
+
+	return nil
+}
 
 func DeleteContainer(
 	containerQueryRepo repository.ContainerQueryRepo,
@@ -17,26 +65,15 @@ func DeleteContainer(
 	accId valueObject.AccountId,
 	containerId valueObject.ContainerId,
 ) error {
-	_, err := containerQueryRepo.GetById(containerId)
+	containerEntity, err := containerQueryRepo.GetById(containerId)
 	if err != nil {
 		log.Printf("ContainerNotFound: %s", err)
 		return errors.New("ContainerNotFound")
 	}
 
-	targets, err := mappingQueryRepo.GetTargetsByContainerId(containerId)
+	err = mappingsJanitor(mappingQueryRepo, mappingCmdRepo, containerEntity)
 	if err != nil {
-		log.Printf("GetTargetByContainerIdError: %s", err)
-		return errors.New("GetTargetByContainerIdInfraError")
-	}
-
-	for _, target := range targets {
-		err = mappingCmdRepo.DeleteTarget(target.Id)
-		if err != nil {
-			log.Printf("DeleteTargetError: %s", err)
-			return errors.New("DeleteTargetInfraError")
-		}
-
-		log.Printf("TargetId '%v' deleted.", target.Id)
+		return err
 	}
 
 	err = containerCmdRepo.Delete(accId, containerId)
