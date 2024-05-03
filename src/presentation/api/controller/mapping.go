bc5ca408365607a3767d3f8627c6af30bd41efc3
ntorga <northontorga@gmail.com>
2024-05-03 17:58:21 +0000

    refactor: use service for mapping api controller

--- src/presentation/api/controller/mapping.go
+++ src/presentation/api/controller/mapping.go
@@ -4,10 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/speedianet/control/src/domain/dto"
-	"github.com/speedianet/control/src/domain/useCase"
 	"github.com/speedianet/control/src/domain/valueObject"
-	"github.com/speedianet/control/src/infra"
 	"github.com/speedianet/control/src/infra/db"
 	apiHelper "github.com/speedianet/control/src/presentation/api/helper"
 	"github.com/speedianet/control/src/presentation/service"
@@ -35,14 +32,7 @@ func NewMappingController(
 // @Success      200 {array} entity.Mapping
 // @Router       /v1/mapping/ [get]
 func (controller *MappingController) Read(c echo.Context) error {
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	mappingQueryRepo := infra.NewMappingQueryRepo(persistentDbSvc)
-	mappingList, err := useCase.GetMappings(mappingQueryRepo)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusInternalServerError, err.Error())
-	}
-
-	return apiHelper.ResponseWrapper(c, http.StatusOK, mappingList)
+	return apiHelper.ServiceResponseWrapper(c, controller.mappingService.Read())
 }
 
 // CreateMapping	 godoc
@@ -61,57 +51,36 @@ func (controller *MappingController) Create(c echo.Context) error {
 		return err
 	}
 
-	requiredParams := []string{"accountId", "publicPort", "containerIds"}
-	apiHelper.CheckMissingParams(requestBody, requiredParams)
-
-	accId := valueObject.NewAccountIdPanic(requestBody["accountId"])
-
-	var hostnamePtr *valueObject.Fqdn
-	if requestBody["hostname"] != nil {
-		hostname := valueObject.NewFqdnPanic(requestBody["hostname"].(string))
-		hostnamePtr = &hostname
-	}
-
-	publicPort := valueObject.NewNetworkPortPanic(requestBody["publicPort"])
-
-	protocol := valueObject.GuessNetworkProtocolByPort(publicPort)
-	if requestBody["protocol"] != nil {
-		protocol = valueObject.NewNetworkProtocolPanic(requestBody["protocol"].(string))
-	}
-
 	containerIds := []valueObject.ContainerId{}
-	for _, targetStr := range requestBody["containerIds"].([]interface{}) {
-		containerId, err := valueObject.NewContainerId(targetStr.(string))
-		if err != nil {
-			return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
+	if requestBody["containerId"] != nil {
+		requestBody["containerIds"] = requestBody["containerId"]
+	}
+
+	if requestBody["containerIds"] != nil {
+		_, isContainerIdsString := requestBody["containerIds"].(string)
+		if isContainerIdsString {
+			requestBody["containerIds"] = []interface{}{requestBody["containerIds"]}
 		}
-		containerIds = append(containerIds, containerId)
+
+		containerIdsSlice, assertOk := requestBody["containerIds"].([]interface{})
+		if !assertOk {
+			return apiHelper.ResponseWrapper(
+				c, http.StatusBadRequest, "ContainerIdsMustBeArray",
+			)
+		}
+
+		for _, rawContainerId := range containerIdsSlice {
+			containerId, err := valueObject.NewContainerId(rawContainerId)
+			if err != nil {
+				return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
+			}
+			containerIds = append(containerIds, containerId)
+		}
 	}
 
-	createMappingDto := dto.NewCreateMapping(
-		accId,
-		hostnamePtr,
-		publicPort,
-		protocol,
-		containerIds,
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.mappingService.Create(requestBody),
 	)
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	mappingQueryRepo := infra.NewMappingQueryRepo(persistentDbSvc)
-	mappingCmdRepo := infra.NewMappingCmdRepo(persistentDbSvc)
-	containerQueryRepo := infra.NewContainerQueryRepo(persistentDbSvc)
-
-	err = useCase.CreateMapping(
-		mappingQueryRepo,
-		mappingCmdRepo,
-		containerQueryRepo,
-		createMappingDto,
-	)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
-	}
-
-	return apiHelper.ResponseWrapper(c, http.StatusCreated, "MappingCreated")
 }
 
 // DeleteMapping godoc
@@ -125,22 +94,13 @@ func (controller *MappingController) Create(c echo.Context) error {
 // @Success      200 {object} object{} "MappingDeleted"
 // @Router       /v1/mapping/{mappingId}/ [delete]
 func (controller *MappingController) Delete(c echo.Context) error {
-	mappingId := valueObject.NewMappingIdPanic(c.Param("mappingId"))
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	mappingQueryRepo := infra.NewMappingQueryRepo(persistentDbSvc)
-	mappingCmdRepo := infra.NewMappingCmdRepo(persistentDbSvc)
-
-	err := useCase.DeleteMapping(
-		mappingQueryRepo,
-		mappingCmdRepo,
-		mappingId,
-	)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
+	requestBody := map[string]interface{}{
+		"mappingId": c.Param("mappingId"),
 	}
 
-	return apiHelper.ResponseWrapper(c, http.StatusOK, "MappingDeleted")
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.mappingService.Delete(requestBody),
+	)
 }
 
 // CreateMappingTarget godoc
@@ -159,33 +119,9 @@ func (controller *MappingController) CreateTarget(c echo.Context) error {
 		return err
 	}
 
-	requiredParams := []string{"mappingId", "containerId"}
-	apiHelper.CheckMissingParams(requestBody, requiredParams)
-
-	mappingId := valueObject.NewMappingIdPanic(requestBody["mappingId"])
-	containerId := valueObject.NewContainerIdPanic(requestBody["containerId"].(string))
-
-	createTargetDto := dto.NewCreateMappingTarget(
-		mappingId,
-		containerId,
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.mappingService.CreateTarget(requestBody),
 	)
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	mappingQueryRepo := infra.NewMappingQueryRepo(persistentDbSvc)
-	mappingCmdRepo := infra.NewMappingCmdRepo(persistentDbSvc)
-	containerQueryRepo := infra.NewContainerQueryRepo(persistentDbSvc)
-
-	err = useCase.CreateMappingTarget(
-		mappingQueryRepo,
-		mappingCmdRepo,
-		containerQueryRepo,
-		createTargetDto,
-	)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
-	}
-
-	return apiHelper.ResponseWrapper(c, http.StatusCreated, "MappingTargetCreated")
 }
 
 // DeleteMappingTarget godoc
@@ -200,20 +136,12 @@ func (controller *MappingController) CreateTarget(c echo.Context) error {
 // @Success      200 {object} object{} "MappingTargetDeleted"
 // @Router       /v1/mapping/{mappingId}/target/{targetId}/ [delete]
 func (controller *MappingController) DeleteTarget(c echo.Context) error {
-	targetId := valueObject.NewMappingTargetIdPanic(c.Param("targetId"))
-
-	persistentDbSvc := c.Get("persistentDbSvc").(*db.PersistentDatabaseService)
-	mappingQueryRepo := infra.NewMappingQueryRepo(persistentDbSvc)
-	mappingCmdRepo := infra.NewMappingCmdRepo(persistentDbSvc)
-
-	err := useCase.DeleteMappingTarget(
-		mappingQueryRepo,
-		mappingCmdRepo,
-		targetId,
-	)
-	if err != nil {
-		return apiHelper.ResponseWrapper(c, http.StatusBadRequest, err.Error())
+	requestBody := map[string]interface{}{
+		"mappingId": c.Param("mappingId"),
+		"targetId":  c.Param("targetId"),
 	}
 
-	return apiHelper.ResponseWrapper(c, http.StatusOK, "MappingTargetDeleted")
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.mappingService.DeleteTarget(requestBody),
+	)
 }
