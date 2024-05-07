f070c10b0d4821e530e55ddd98edfd6836292950
ntorga <northontorga@gmail.com>
2024-05-07 20:43:45 +0000

    test: split create and delete dummy containers into funcs

--- src/infra/containerCmdRepo_test.go
+++ src/infra/containerCmdRepo_test.go
@@ -9,6 +9,67 @@ import (
 	"github.com/speedianet/control/src/domain/valueObject"
 )
 
+func createDummyContainer(containerCmdRepo *ContainerCmdRepo) error {
+	portBindings, _ := valueObject.NewPortBindingFromString("http")
+
+	restartPolicy := valueObject.NewContainerRestartPolicyPanic("unless-stopped")
+
+	profileId := valueObject.NewContainerProfileIdPanic(0)
+
+	envs := []valueObject.ContainerEnv{
+		valueObject.NewContainerEnvPanic("CONTROL_ENV1=testing"),
+		valueObject.NewContainerEnvPanic("CONTROL_ENV2=testing"),
+	}
+
+	accountId := valueObject.NewAccountIdPanic(os.Getenv("DUMMY_USER_ID"))
+
+	launchScript, err := valueObject.NewLaunchScript(
+		"printenv > /tmp/hello.txt",
+	)
+	if err != nil {
+		return err
+	}
+
+	createContainer := dto.NewCreateContainer(
+		accountId,
+		valueObject.NewFqdnPanic("speedia.net"),
+		valueObject.NewContainerImageAddressPanic("https://docker.io/speedianet/os"),
+		portBindings,
+		&restartPolicy,
+		nil,
+		&profileId,
+		envs,
+		&launchScript,
+		false,
+	)
+
+	_, err = containerCmdRepo.Create(createContainer)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deleteDummyContainer(
+	containerQueryRepo *ContainerQueryRepo,
+	containerCmdRepo *ContainerCmdRepo,
+) error {
+	containers, err := containerQueryRepo.Get()
+	if err != nil {
+		return err
+	}
+
+	if len(containers) == 0 {
+		return nil
+	}
+
+	return containerCmdRepo.Delete(
+		containers[0].AccountId,
+		containers[0].Id,
+	)
+}
+
 func TestContainerCmdRepo(t *testing.T) {
 	testHelpers.LoadEnvVars()
 	persistentDbSvc := testHelpers.GetPersistentDbSvc()
@@ -16,42 +77,10 @@ func TestContainerCmdRepo(t *testing.T) {
 	containerCmdRepo := NewContainerCmdRepo(persistentDbSvc)
 
 	t.Run("CreateContainer", func(t *testing.T) {
-		portBindings, _ := valueObject.NewPortBindingFromString("http")
-
-		restartPolicy := valueObject.NewContainerRestartPolicyPanic("unless-stopped")
-
-		profileId := valueObject.NewContainerProfileIdPanic(0)
-
-		envs := []valueObject.ContainerEnv{
-			valueObject.NewContainerEnvPanic("CONTROL_ENV1=testing"),
-			valueObject.NewContainerEnvPanic("CONTROL_ENV2=testing"),
-		}
-
-		accountId := valueObject.NewAccountIdPanic(os.Getenv("DUMMY_USER_ID"))
-
-		launchScript, err := valueObject.NewLaunchScript(
-			"printenv > /tmp/hello.txt",
-		)
-		if err != nil {
-			t.Errorf("ExpectedNoErrorButGot: %v", err)
-		}
-
-		createContainer := dto.NewCreateContainer(
-			accountId,
-			valueObject.NewFqdnPanic("speedia.net"),
-			valueObject.NewContainerImageAddressPanic("https://docker.io/nginx:latest"),
-			portBindings,
-			&restartPolicy,
-			nil,
-			&profileId,
-			envs,
-			&launchScript,
-			false,
-		)
-
-		_, err = containerCmdRepo.Create(createContainer)
+		err := createDummyContainer(containerCmdRepo)
 		if err != nil {
 			t.Errorf("CreateContainerFailed: %v", err)
+			return
 		}
 	})
 
@@ -80,20 +109,7 @@ func TestContainerCmdRepo(t *testing.T) {
 	})
 
 	t.Run("DeleteContainer", func(t *testing.T) {
-		accId := valueObject.NewAccountIdPanic(os.Getenv("DUMMY_USER_ID"))
-		containers, err := containerQueryRepo.GetByAccId(accId)
-		if err != nil {
-			t.Errorf("GetContainersFailed: %v", err)
-		}
-
-		if len(containers) == 0 {
-			t.Error("NoContainersFound")
-		}
-
-		err = containerCmdRepo.Delete(
-			accId,
-			containers[0].Id,
-		)
+		err := deleteDummyContainer(containerQueryRepo, containerCmdRepo)
 		if err != nil {
 			t.Errorf("DeleteContainerFailed: %v", err)
 		}
