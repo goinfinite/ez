dfebcd1a38755817cf3093c02c396571a9252947
ntorga <northontorga@gmail.com>
2024-05-07 20:44:01 +0000

    feat: add proxy model constructor

--- src/infra/db/model/containerProxy.go
+++ src/infra/db/model/containerProxy.go
@@ -13,3 +13,20 @@ type ContainerProxy struct {
 func (ContainerProxy) TableName() string {
 	return "container_proxies"
 }
+
+func NewContainerProxy(
+	id uint,
+	containerId string,
+	containerHostname string,
+) ContainerProxy {
+	proxyModel := ContainerProxy{
+		ContainerId:       containerId,
+		ContainerHostname: containerHostname,
+	}
+
+	if id != 0 {
+		proxyModel.ID = id
+	}
+
+	return proxyModel
+}
