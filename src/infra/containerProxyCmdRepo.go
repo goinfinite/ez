f90b53bc33bb9435a6de344e8f3a1b544d1ebfe7
ntorga <northontorga@gmail.com>
2024-05-07 20:44:38 +0000

    feat: init create and delete impl with db

--- src/infra/containerProxyCmdRepo.go
+++ src/infra/containerProxyCmdRepo.go
@@ -3,6 +3,7 @@ package infra
 import (
 	"github.com/speedianet/control/src/domain/valueObject"
 	"github.com/speedianet/control/src/infra/db"
+	dbModel "github.com/speedianet/control/src/infra/db/model"
 )
 
 type ContainerProxyCmdRepo struct {
@@ -19,10 +20,38 @@ func NewContainerProxyCmdRepo(
 	}
 }
 
-func (repo *ContainerProxyCmdRepo) Create(containerId valueObject.ContainerId) error {
+func (repo *ContainerProxyCmdRepo) updateWebServerFile() error {
 	return nil
 }
 
+func (repo *ContainerProxyCmdRepo) Create(containerId valueObject.ContainerId) error {
+	containerEntity, err := repo.containerQueryRepo.GetById(containerId)
+	if err != nil {
+		return err
+	}
+
+	proxyModel := dbModel.NewContainerProxy(
+		0,
+		containerEntity.Id.String(),
+		containerEntity.Hostname.String(),
+	)
+
+	createResult := repo.persistentDbSvc.Handler.Create(&proxyModel)
+	if createResult.Error != nil {
+		return createResult.Error
+	}
+
+	return repo.updateWebServerFile()
+}
+
 func (repo *ContainerProxyCmdRepo) Delete(containerId valueObject.ContainerId) error {
-	return nil
+	err := repo.persistentDbSvc.Handler.Delete(
+		dbModel.ContainerProxy{},
+		"container_id = ?", containerId.String(),
+	).Error
+	if err != nil {
+		return err
+	}
+
+	return repo.updateWebServerFile()
 }
