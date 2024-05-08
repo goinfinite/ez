c4a1a7eed94d628967f11f255910cc3d248a1b5e
ntorga <northontorga@gmail.com>
2024-05-08 18:31:26 +0000

    feat: impl actual nginx config factory

--- src/infra/containerProxyCmdRepo.go
+++ src/infra/containerProxyCmdRepo.go
@@ -1,10 +1,17 @@
 package infra
 
 import (
+	"errors"
+	"html/template"
+	"strings"
+
 	"github.com/speedianet/control/src/domain/valueObject"
 	"github.com/speedianet/control/src/infra/db"
 	dbModel "github.com/speedianet/control/src/infra/db/model"
+	infraHelper "github.com/speedianet/control/src/infra/helper"
 )
+
+const ProxyWebServerFile = "/var/nginx/http.d/container-proxy.conf"
 
 type ContainerProxyCmdRepo struct {
 	persistentDbSvc    *db.PersistentDatabaseService
@@ -21,6 +28,66 @@ func NewContainerProxyCmdRepo(
 }
 
 func (repo *ContainerProxyCmdRepo) updateWebServerFile() error {
+	proxyModels := []dbModel.ContainerProxy{}
+	err := repo.persistentDbSvc.Handler.Find(&proxyModels).Error
+	if err != nil {
+		return err
+	}
+
+	controlHostname := "replace-me"
+	fileTemplate := `server {
+	listen 1618 ssl;
+	server_name ` + controlHostname + `;
+
+	ssl_certificate /var/speedia/pki/control.crt;
+	ssl_certificate_key /var/speedia/pki/control.key;
+
+	{{ range . }}
+		location /{{ .ContainerId }}/ {
+			proxy_pass https://localhost:{{ .ContainerPrivatePort }};
+		}
+	{{ end }}
+}
+
+{{ range . }}
+server {
+	listen 1618 ssl;
+	server_name {{ .ContainerHostname}} os.{{ .ContainerHostname}};
+
+	ssl_certificate /var/speedia/pki/control.crt;
+	ssl_certificate_key /var/speedia/pki/control.key;
+
+	location /{
+		proxy_pass https://localhost:{{ .ContainerPrivatePort }};
+	}
+}
+{{ end }}`
+
+	if len(proxyModels) == 0 {
+		fileTemplate = ``
+	}
+
+	templatePtr, err := template.New("webServerFile").Parse(fileTemplate)
+	if err != nil {
+		return errors.New("TemplateParsingError: " + err.Error())
+	}
+
+	var webServerFileContent strings.Builder
+	err = templatePtr.Execute(&webServerFileContent, proxyModels)
+	if err != nil {
+		return errors.New("TemplateExecutionError: " + err.Error())
+	}
+
+	err = infraHelper.UpdateFile(ProxyWebServerFile, webServerFileContent.String(), true)
+	if err != nil {
+		return err
+	}
+
+	_, err = infraHelper.RunCmdWithSubShell("nginx -t && nginx -s reload")
+	if err != nil {
+		return errors.New("ReloadNginxFailed: " + err.Error())
+	}
+
 	return nil
 }
 
@@ -30,10 +97,23 @@ func (repo *ContainerProxyCmdRepo) Create(containerId valueObject.ContainerId) e
 		return err
 	}
 
+	containerPrivatePort := uint64(0)
+	for _, portBinding := range containerEntity.PortBindings {
+		if portBinding.ContainerPort.String() != "1618" {
+			continue
+		}
+
+		containerPrivatePort = portBinding.PrivatePort.Get()
+	}
+	if containerPrivatePort == 0 {
+		return errors.New("SpeediaOsPrivatePortNotFound")
+	}
+
 	proxyModel := dbModel.NewContainerProxy(
 		0,
 		containerEntity.Id.String(),
 		containerEntity.Hostname.String(),
+		uint(containerPrivatePort),
 	)
 
 	createResult := repo.persistentDbSvc.Handler.Create(&proxyModel)
