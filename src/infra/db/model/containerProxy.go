4a64c9ebf6eeb031e298f9736ec9e8f84178bc65
ntorga <northontorga@gmail.com>
2024-05-08 18:30:58 +0000

    feat: add container private port

--- src/infra/db/model/containerProxy.go
+++ src/infra/db/model/containerProxy.go
@@ -3,11 +3,12 @@ package dbModel
 import "time"
 
 type ContainerProxy struct {
-	ID                uint `gorm:"primarykey"`
-	ContainerId       string
-	ContainerHostname string
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
+	ID                   uint `gorm:"primarykey"`
+	ContainerId          string
+	ContainerHostname    string
+	ContainerPrivatePort uint
+	CreatedAt            time.Time
+	UpdatedAt            time.Time
 }
 
 func (ContainerProxy) TableName() string {
@@ -18,10 +19,12 @@ func NewContainerProxy(
 	id uint,
 	containerId string,
 	containerHostname string,
+	containerPrivatePort uint,
 ) ContainerProxy {
 	proxyModel := ContainerProxy{
-		ContainerId:       containerId,
-		ContainerHostname: containerHostname,
+		ContainerId:          containerId,
+		ContainerHostname:    containerHostname,
+		ContainerPrivatePort: containerPrivatePort,
 	}
 
 	if id != 0 {
