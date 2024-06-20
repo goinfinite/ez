0c83b39df68a65c7dafa04f61f74d2293c5d8424
ntorga <northontorga@gmail.com>
2024-06-20 20:40:02 +0000

    fix: add missing String() to unix time

--- src/domain/valueObject/unixTime.go
+++ src/domain/valueObject/unixTime.go
@@ -4,18 +4,22 @@ import "time"
 
 type UnixTime int64
 
-func (ut UnixTime) Get() int64 {
-	return time.Unix(int64(ut), 0).UTC().Unix()
+func (vo UnixTime) Get() int64 {
+	return time.Unix(int64(vo), 0).UTC().Unix()
 }
 
-func (ut UnixTime) GetRfcDate() string {
-	return time.Unix(int64(ut), 0).UTC().Format(time.RFC3339)
+func (vo UnixTime) GetRfcDate() string {
+	return time.Unix(int64(vo), 0).UTC().Format(time.RFC3339)
 }
 
-func (ut UnixTime) GetDateOnly() string {
-	return time.Unix(int64(ut), 0).UTC().Format("2006-01-02")
+func (vo UnixTime) GetDateOnly() string {
+	return time.Unix(int64(vo), 0).UTC().Format("2006-01-02")
 }
 
-func (ut UnixTime) GetTimeOnly() string {
-	return time.Unix(int64(ut), 0).UTC().Format("15:04:05")
+func (vo UnixTime) GetTimeOnly() string {
+	return time.Unix(int64(vo), 0).UTC().Format("15:04:05")
+}
+
+func (vo UnixTime) String() string {
+	return time.Unix(int64(vo), 0).UTC().String()
 }
