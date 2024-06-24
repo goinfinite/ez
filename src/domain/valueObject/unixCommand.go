d4b765484311e164536672584963df92d06f9494
ntorga <northontorga@gmail.com>
2024-06-24 22:56:02 +0000

    feat: add scheduled task entity and vos

---
+++ src/domain/valueObject/unixCommand.go
@@ -0,0 +1,35 @@
+package valueObject
+
+import (
+	"errors"
+	"strings"
+
+	voHelper "github.com/speedianet/control/src/domain/valueObject/helper"
+)
+
+type UnixCommand string
+
+func NewUnixCommand(value interface{}) (UnixCommand, error) {
+	stringValue, err := voHelper.InterfaceToString(value)
+	if err != nil {
+		return "", errors.New("UnixCommandMustBeString")
+	}
+
+	stringValue = strings.TrimSpace(stringValue)
+
+	valueLength := len(stringValue)
+	switch {
+	case valueLength == 0:
+		return "", errors.New("UnixCommandEmpty")
+	case valueLength < 3:
+		return "", errors.New("UnixCommandTooShort")
+	case valueLength > 4096:
+		return "", errors.New("UnixCommandTooLong")
+	}
+
+	return UnixCommand(stringValue), nil
+}
+
+func (vo UnixCommand) String() string {
+	return string(vo)
+}
