848ac97886269da4712efb4e21a1fcf90c5f991c
ntorga <northontorga@gmail.com>
2024-07-02 21:28:07 +0000

    feat: add trail db svc infra

---
+++ src/infra/db/trailDbService.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"errors"
+
+	"github.com/glebarez/sqlite"
+	dbModel "github.com/speedianet/control/src/infra/db/model"
+	"gorm.io/gorm"
+)
+
+type TrailDatabaseService struct {
+	Handler *gorm.DB
+}
+
+func NewTrailDatabaseService() (*TrailDatabaseService, error) {
+	ormSvc, err := gorm.Open(
+		sqlite.Open("/var/speedia/trail.db"),
+		&gorm.Config{},
+	)
+	if err != nil {
+		return nil, errors.New("DatabaseConnectionError")
+	}
+
+	dbSvc := &TrailDatabaseService{Handler: ormSvc}
+	err = dbSvc.RunMigrations()
+	if err != nil {
+		return nil, err
+	}
+
+	return dbSvc, nil
+}
+
+func (service *TrailDatabaseService) RunMigrations() error {
+	err := service.Handler.AutoMigrate(
+		&dbModel.SecurityEvent{},
+	)
+	if err != nil {
+		return errors.New("TrailDatabaseMigrationError")
+	}
+
+	return nil
+}
