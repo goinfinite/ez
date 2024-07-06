f6460f16091c83c95b2fbbac2dc1d3b9f9ec8d2e
ntorga <northontorga@gmail.com>
2024-07-06 21:23:32 +0000

    fix: uint reflect

--- src/domain/valueObject/helper/interfaceToInt64.go
+++ src/domain/valueObject/helper/interfaceToInt64.go
@@ -15,7 +15,8 @@ func InterfaceToInt64(input interface{}) (output int64, err error) {
 		intValue := reflect.ValueOf(v).Int()
 		output = int64(intValue)
 	case uint, uint8, uint16, uint32, uint64:
-		output = int64(reflect.ValueOf(v).Int())
+		uintValue := reflect.ValueOf(v).Uint()
+		output = int64(uintValue)
 	case float32, float64:
 		floatValue := reflect.ValueOf(v).Float()
 		output = int64(floatValue)
