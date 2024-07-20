68d82454d2c359507c5564586aeccc84df18267c
ntorga <northontorga@gmail.com>
2024-07-20 18:05:15 +0000

    fix: remove panic from host min capacity

--- src/domain/valueObject/hostMinCapacity.go
+++ src/domain/valueObject/hostMinCapacity.go
@@ -10,34 +10,26 @@ import (
 type HostMinCapacity float64
 
 func NewHostMinCapacity(value interface{}) (HostMinCapacity, error) {
-	hmc, err := voHelper.InterfaceToFloat(value)
+	floatValue, err := voHelper.InterfaceToFloat(value)
 	if err != nil {
 		return 0, errors.New("InvalidHostMinCapacity")
 	}
 
-	if hmc < 0 || hmc > 100 {
+	if floatValue < 0 || floatValue > 100 {
 		return 0, errors.New("HostMinCapacityInvalidRange")
 	}
 
-	return HostMinCapacity(hmc), nil
+	return HostMinCapacity(floatValue), nil
 }
 
 func DefaultHostMinCapacity() HostMinCapacity {
 	return HostMinCapacity(20)
 }
 
-func NewHostMinCapacityPanic(value interface{}) HostMinCapacity {
-	hmc, err := NewHostMinCapacity(value)
-	if err != nil {
-		panic(err)
-	}
-	return hmc
+func (vo HostMinCapacity) Float64() float64 {
+	return float64(vo)
 }
 
-func (hmc HostMinCapacity) Read() float64 {
-	return float64(hmc)
-}
-
-func (hmc HostMinCapacity) String() string {
-	return strconv.FormatFloat(float64(hmc), 'f', -1, 64)
+func (vo HostMinCapacity) String() string {
+	return strconv.FormatFloat(float64(vo), 'f', -1, 64)
 }
