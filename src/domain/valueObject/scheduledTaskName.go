f6a131afdad40294c696d45ddc7f21d7ca402ed4
ntorga <northontorga@gmail.com>
2024-07-22 16:12:17 +0000

    fix: simplify schedule task name construct

--- src/domain/valueObject/scheduledTaskName.go
+++ src/domain/valueObject/scheduledTaskName.go
@@ -3,7 +3,6 @@ package valueObject
 import (
 	"errors"
 	"regexp"
-	"strings"
 
 	voHelper "github.com/speedianet/control/src/domain/valueObject/helper"
 )
@@ -12,18 +11,15 @@ const scheduledTaskNameRegex string = `^[a-zA-Z][\w\-]{1,256}[\w\-\ ]{0,512}$`
 
 type ScheduledTaskName string
 
-func NewScheduledTaskName(value interface{}) (ScheduledTaskName, error) {
+func NewScheduledTaskName(value interface{}) (name ScheduledTaskName, err error) {
 	stringValue, err := voHelper.InterfaceToString(value)
 	if err != nil {
-		return "", errors.New("ScheduledTaskNameMustBeString")
+		return name, errors.New("ScheduledTaskNameMustBeString")
 	}
 
-	stringValue = strings.TrimSpace(stringValue)
-
 	re := regexp.MustCompile(scheduledTaskNameRegex)
-	isValid := re.MatchString(stringValue)
-	if !isValid {
-		return "", errors.New("InvalidScheduledTaskName")
+	if !re.MatchString(stringValue) {
+		return name, errors.New("InvalidScheduledTaskName")
 	}
 
 	return ScheduledTaskName(stringValue), nil
