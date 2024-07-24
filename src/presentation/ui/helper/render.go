8e162084389d4c0cea45340e08481087d2487795
ntorga <northontorga@gmail.com>
2024-07-24 18:15:47 +0000

    feat: pass current url to main layout and install ph icons

--- src/presentation/ui/helper/render.go
+++ src/presentation/ui/helper/render.go
@@ -10,8 +10,9 @@ import (
 func Render(c echo.Context, pageContent templ.Component, statusCode int) error {
 	c.Response().Writer.WriteHeader(statusCode)
 	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	currentUrl := c.Request().URL.String()
 
-	return layout.MainLayout(pageContent).Render(
+	return layout.MainLayout(pageContent, currentUrl).Render(
 		c.Request().Context(),
 		c.Response().Writer,
 	)
