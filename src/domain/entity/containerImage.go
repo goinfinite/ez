019399d3691f23a342268c60c88f8b0d857cc238
ntorga <northontorga@gmail.com>
2024-08-28 16:55:32 +0000

    fix: merge attrs into entity

--- src/domain/entity/containerImage.go
+++ src/domain/entity/containerImage.go
@@ -3,13 +3,15 @@ package entity
 import "github.com/speedianet/control/src/domain/valueObject"
 
 type ContainerImage struct {
-	Id                 valueObject.ContainerImageId             `json:"id"`
-	ImageAddress       valueObject.ContainerImageAddress        `json:"imageAddress"`
-	ImageHash          valueObject.Hash                         `json:"imageHash"`
-	Isa                valueObject.InstructionSetArchitecture   `json:"isa"`
-	SizeBytes          valueObject.Byte                         `json:"sizeBytes"`
-	SnapshotAttributes *valueObject.ContainerSnapshotAttributes `json:"snapshotAttributes"`
-	CreatedAt          valueObject.UnixTime                     `json:"createdAt"`
+	Id           valueObject.ContainerImageId           `json:"id"`
+	ImageAddress valueObject.ContainerImageAddress      `json:"imageAddress"`
+	ImageHash    valueObject.Hash                       `json:"imageHash"`
+	Isa          valueObject.InstructionSetArchitecture `json:"isa"`
+	SizeBytes    valueObject.Byte                       `json:"sizeBytes"`
+	PortBindings []valueObject.PortBinding              `json:"portBindings"`
+	Envs         []valueObject.ContainerEnv             `json:"envs"`
+	Entrypoint   *valueObject.ContainerEntrypoint       `json:"entrypoint"`
+	CreatedAt    valueObject.UnixTime                   `json:"createdAt"`
 }
 
 func NewContainerImage(
@@ -18,16 +20,20 @@ func NewContainerImage(
 	imageHash valueObject.Hash,
 	isa valueObject.InstructionSetArchitecture,
 	sizeBytes valueObject.Byte,
-	snapshotAttributes *valueObject.ContainerSnapshotAttributes,
+	portBindings []valueObject.PortBinding,
+	envs []valueObject.ContainerEnv,
+	entrypoint *valueObject.ContainerEntrypoint,
 	createdAt valueObject.UnixTime,
 ) ContainerImage {
 	return ContainerImage{
-		Id:                 id,
-		ImageAddress:       imageAddress,
-		ImageHash:          imageHash,
-		Isa:                isa,
-		SizeBytes:          sizeBytes,
-		SnapshotAttributes: snapshotAttributes,
-		CreatedAt:          createdAt,
+		Id:           id,
+		ImageAddress: imageAddress,
+		ImageHash:    imageHash,
+		Isa:          isa,
+		SizeBytes:    sizeBytes,
+		PortBindings: portBindings,
+		Envs:         envs,
+		Entrypoint:   entrypoint,
+		CreatedAt:    createdAt,
 	}
 }
