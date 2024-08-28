a473427151cd3b31e8b8079077155d994c94d99a
ntorga <northontorga@gmail.com>
2024-08-28 20:12:17 +0000

    feat: impl image delete

---
+++ src/infra/containerImageCmdRepo.go
@@ -0,0 +1,30 @@
+package infra
+
+import (
+	"github.com/speedianet/control/src/domain/dto"
+	"github.com/speedianet/control/src/infra/db"
+	infraHelper "github.com/speedianet/control/src/infra/helper"
+)
+
+type ContainerImageCmdRepo struct {
+	persistentDbSvc *db.PersistentDatabaseService
+}
+
+func NewContainerImageCmdRepo(
+	persistentDbSvc *db.PersistentDatabaseService,
+) *ContainerImageCmdRepo {
+	return &ContainerImageCmdRepo{persistentDbSvc: persistentDbSvc}
+}
+
+func (repo *ContainerImageCmdRepo) Delete(
+	deleteDto dto.DeleteContainerImage,
+) error {
+	_, err := infraHelper.RunCmdAsUser(
+		deleteDto.AccountId, "podman", "image", "rm", deleteDto.ImageId.String(),
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
