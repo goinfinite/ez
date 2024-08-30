176db3c21ba04189493dfcef194808dbb90627f0
ntorga <northontorga@gmail.com>
2024-08-30 22:09:08 +0000

    fix: add export endpoint to router

--- src/presentation/api/router.go
+++ src/presentation/api/router.go
@@ -94,6 +94,7 @@ func (router *Router) containerRoutes() {
 	)
 	containerImageGroup.GET("/", containerImageController.Read)
 	containerImageGroup.POST("/snapshot/", containerImageController.CreateSnapshot)
+	containerImageGroup.POST("/export/", containerImageController.Export)
 	containerImageGroup.DELETE("/:imageId/", containerImageController.Delete)
 }
 
