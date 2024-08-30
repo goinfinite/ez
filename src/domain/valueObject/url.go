0daf7038dc5a88688cd0717d4c80c84255332233
ntorga <northontorga@gmail.com>
2024-08-30 22:05:15 +0000

    refactor: url vo

--- src/domain/valueObject/url.go
+++ src/domain/valueObject/url.go
@@ -3,38 +3,33 @@ package valueObject
 import (
 	"errors"
 	"regexp"
+	"strings"
+
+	voHelper "github.com/speedianet/control/src/domain/valueObject/helper"
 )
 
-const urlRegex string = `^(?P<schema>https?:\/\/)(?P<hostname>[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9][a-z0-9-]{0,61}[a-z0-9])*)(:(?P<port>\d{1,6}))?(?P<path>\/[a-z0-9\/\_\.\-]*)?(?P<query>\?[\w\/#=&%\-]*)?$`
+const urlRegexExpression string = `^(?P<schema>https?:\/\/)(?P<hostname>[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9][a-z0-9-]{0,61}[a-z0-9])*)(:(?P<port>\d{1,6}))?(?P<path>\/[A-Za-z0-9\/\_\.\-]*)?(?P<query>\?[\w\/#=&%\-]*)?$`
 
 type Url string
 
-func NewUrl(value string) (Url, error) {
-	hasScheme := regexp.MustCompile(`^(http|https)://`)
-	if !hasScheme.MatchString(value) {
-		value = "https://" + value
-	}
-
-	url := Url(value)
-	if !url.isValid(value) {
-		return "", errors.New("InvalidUrl")
-	}
-	return url, nil
-}
-
-func NewUrlPanic(value string) Url {
-	url, err := NewUrl(value)
+func NewUrl(value interface{}) (url Url, err error) {
+	stringValue, err := voHelper.InterfaceToString(value)
 	if err != nil {
-		panic(err)
+		return url, errors.New("UrlValueMustBeString")
 	}
-	return url
+
+	if !strings.HasPrefix(stringValue, "http") {
+		stringValue = "https://" + stringValue
+	}
+
+	re := regexp.MustCompile(urlRegexExpression)
+	if !re.MatchString(stringValue) {
+		return url, errors.New("InvalidUrl")
+	}
+
+	return Url(stringValue), nil
 }
 
-func (Url) isValid(value string) bool {
-	re := regexp.MustCompile(urlRegex)
-	return re.MatchString(value)
-}
-
-func (url Url) String() string {
-	return string(url)
+func (vo Url) String() string {
+	return string(vo)
 }
