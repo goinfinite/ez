551a6d6fc4ca836a237534839c09e2f4d91b7116
ntorga <northontorga@gmail.com>
2024-09-28 15:03:42 +0000

    feat: impl filters and sorts to get scheduled task controller

--- src/presentation/api/controller/scheduledTask.go
+++ src/presentation/api/controller/scheduledTask.go
@@ -1,10 +1,13 @@
 package apiController
 
 import (
+	"log/slog"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/speedianet/control/src/domain/useCase"
+	"github.com/speedianet/control/src/domain/valueObject"
 	"github.com/speedianet/control/src/infra"
 	"github.com/speedianet/control/src/infra/db"
 	apiHelper "github.com/speedianet/control/src/presentation/api/helper"
@@ -25,6 +28,22 @@ func NewScheduledTaskController(
 	}
 }
 
+func (controller *ScheduledTaskController) parseTaskTags(
+	rawTags string,
+) []valueObject.ScheduledTaskTag {
+	taskTags := []valueObject.ScheduledTaskTag{}
+	for rawTagIndex, rawTag := range strings.Split(rawTags, ";") {
+		taskTag, err := valueObject.NewScheduledTaskTag(rawTag)
+		if err != nil {
+			slog.Debug("InvalidTaskTag", slog.Int("tagIndex", rawTagIndex))
+			continue
+		}
+		taskTags = append(taskTags, taskTag)
+	}
+
+	return taskTags
+}
+
 // ReadScheduledTasks	 godoc
 // @Summary      ReadScheduledTasks
 // @Description  List scheduled tasks.
@@ -32,10 +51,48 @@ func NewScheduledTaskController(
 // @Accept       json
 // @Produce      json
 // @Security     Bearer
-// @Success      200 {array} entity.ScheduledTask
-// @Router       /v1/task/ [get]
+// @Param        taskOd query  string  false  "TaskId"
+// @Param        taskStatus query  string  false  "TaskStatus"
+// @Param        taskTags query  string  false  "TaskTags"
+// @Param        startedBeforeAt query  string  false  "StartedBeforeAt"
+// @Param        startedAfterAt query  string  false  "StartedAfterAt"
+// @Param        finishedBeforeAt query  string  false  "FinishedBeforeAt"
+// @Param        finishedAfterAt query  string  false  "FinishedAfterAt"
+// @Param        createdBeforeAt query  string  false  "CreatedBeforeAt"
+// @Param        createdAfterAt query  string  false  "CreatedAfterAt"
+// @Param        pageNumber query  uint  false  "PageNumber (Pagination)"
+// @Param        itemsPerPage query  uint  false  "ItemsPerPage (Pagination)"
+// @Param        sortBy query  string  false  "SortBy (Pagination)"
+// @Param        sortDirection query  string  false  "SortDirection (Pagination)"
+// @Param        lastSeenId query  string  false  "LastSeenId (Pagination)"
+// @Success      200 {array} dto.ReadScheduledTasksResponse
+// @Router       /v1/scheduled-task/ [get]
 func (controller *ScheduledTaskController) Read(c echo.Context) error {
-	return apiHelper.ServiceResponseWrapper(c, controller.scheduledTaskService.Read())
+	requestBody := map[string]interface{}{}
+	queryParameters := []string{
+		"taskId", "taskStatus", "taskTags",
+		"startedBeforeAt", "startedAfterAt",
+		"finishedBeforeAt", "finishedAfterAt",
+		"createdBeforeAt", "createdAfterAt",
+		"pageNumber", "itemsPerPage", "sortBy", "sortDirection", "lastSeenId",
+	}
+	for _, paramName := range queryParameters {
+		paramValue := c.QueryParam(paramName)
+		if paramValue == "" {
+			continue
+		}
+
+		if paramName != "taskTags" {
+			requestBody[paramName] = paramValue
+			continue
+		}
+
+		requestBody[paramName] = controller.parseTaskTags(paramValue)
+	}
+
+	return apiHelper.ServiceResponseWrapper(
+		c, controller.scheduledTaskService.Read(requestBody),
+	)
 }
 
 // UpdateScheduledTask godoc
@@ -47,7 +104,7 @@ func (controller *ScheduledTaskController) Read(c echo.Context) error {
 // @Security     Bearer
 // @Param        updateScheduledTaskDto 	  body dto.UpdateScheduledTask  true  "UpdateScheduledTask (Only id is required.)"
 // @Success      200 {object} object{} "ScheduledTaskUpdated"
-// @Router       /v1/task/ [put]
+// @Router       /v1/scheduled-task/ [put]
 func (controller *ScheduledTaskController) Update(c echo.Context) error {
 	requestBody, err := apiHelper.ReadRequestBody(c)
 	if err != nil {
