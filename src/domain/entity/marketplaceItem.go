a9c80470b9814754844afe9b4558720cb0acd3dd
ntorga <northontorga@gmail.com>
2024-10-09 21:52:24 +0000

    fix: rename recipe to manifest

--- src/domain/entity/marketplaceItem.go
+++ src/domain/entity/marketplaceItem.go
@@ -3,29 +3,29 @@ package entity
 import "github.com/goinfinite/ez/src/domain/valueObject"
 
 type MarketplaceItem struct {
-	ReceiptVersion     valueObject.MarketplaceItemReceiptVersion `json:"receiptVersion"`
-	Slug               valueObject.MarketplaceItemSlug           `json:"slug"`
-	Name               valueObject.MarketplaceItemName           `json:"name"`
-	Type               valueObject.MarketplaceItemType           `json:"type"`
-	Description        valueObject.MarketplaceItemDescription    `json:"description"`
-	LaunchScript       valueObject.LaunchScript                  `json:"launchScript"`
-	EstimatedSizeBytes valueObject.Byte                          `json:"estimatedSizeBytes"`
-	AvatarUrl          valueObject.Url                           `json:"avatarUrl,omitempty"`
+	ManifestVersion    valueObject.MarketplaceItemManifestVersion `json:"manifestVersion"`
+	Slugs              []valueObject.MarketplaceItemSlug          `json:"slugs"`
+	Name               valueObject.MarketplaceItemName            `json:"name"`
+	Type               valueObject.MarketplaceItemType            `json:"type"`
+	Description        valueObject.MarketplaceItemDescription     `json:"description"`
+	LaunchScript       valueObject.LaunchScript                   `json:"launchScript"`
+	EstimatedSizeBytes *valueObject.Byte                          `json:"estimatedSizeBytes,omitempty"`
+	AvatarUrl          *valueObject.Url                           `json:"avatarUrl,omitempty"`
 }
 
 func NewMarketplaceItem(
-	receiptVersion valueObject.MarketplaceItemReceiptVersion,
-	slug valueObject.MarketplaceItemSlug,
+	manifestVersion valueObject.MarketplaceItemManifestVersion,
+	slugs []valueObject.MarketplaceItemSlug,
 	name valueObject.MarketplaceItemName,
 	itemType valueObject.MarketplaceItemType,
 	description valueObject.MarketplaceItemDescription,
 	launchScript valueObject.LaunchScript,
-	estimatedSizeBytes valueObject.Byte,
-	avatarUrl valueObject.Url,
+	estimatedSizeBytes *valueObject.Byte,
+	avatarUrl *valueObject.Url,
 ) MarketplaceItem {
 	return MarketplaceItem{
-		ReceiptVersion:     receiptVersion,
-		Slug:               slug,
+		ManifestVersion:    manifestVersion,
+		Slugs:              slugs,
 		Name:               name,
 		Type:               itemType,
 		Description:        description,
