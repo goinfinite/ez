76014dddc89e6ae0bea9c4be4e036d72c18c22fd
ntorga <northontorga@gmail.com>
2024-10-09 21:53:13 +0000

    feat: add marketplace dir to envs

--- src/infra/envs/envs.go
+++ src/infra/envs/envs.go
@@ -3,6 +3,7 @@ package infraEnvs
 const (
 	InfiniteEzVersion    string = "0.0.8"
 	InfiniteEzMainDir    string = "/var/infinite"
+	MarketplaceDir       string = InfiniteEzMainDir + "/marketplace"
 	InfiniteEzBinary     string = InfiniteEzMainDir + "/ez"
 	AccessTokenCookieKey string = "control-access-token"
 	UserDataDirectory    string = "/var/data"
