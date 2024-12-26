020fe1cb98caf9f6ed01dd249090f647019a4f9d
ntorga <northontorga@gmail.com>
2024-12-26 16:30:09 +0000

    feat: add read job infra impl

--- src/infra/backup/backupQueryRepo.go
+++ src/infra/backup/backupQueryRepo.go
@@ -88,3 +88,86 @@ func (repo *BackupQueryRepo) ReadDestination(
 		Destinations: backupDestinationEntities,
 	}, nil
 }
+
+func (repo *BackupQueryRepo) ReadJob(
+	readDto dto.ReadBackupJobsRequest,
+) (responseDto dto.ReadBackupJobsResponse, err error) {
+	backupJobEntities := []entity.BackupJob{}
+
+	backupJobModel := dbModel.BackupJob{}
+	if readDto.JobId != nil {
+		backupJobModel.ID = readDto.JobId.Uint64()
+	}
+	if readDto.JobStatus != nil {
+		backupJobModel.JobStatus = *readDto.JobStatus
+	}
+	if readDto.AccountId != nil {
+		backupJobModel.AccountID = readDto.AccountId.Uint64()
+	}
+	if readDto.DestinationId != nil {
+		backupJobModel.DestinationIds = []uint64{readDto.DestinationId.Uint64()}
+	}
+	if readDto.BackupType != nil {
+		backupTypeStr := readDto.BackupType.String()
+		backupJobModel.BackupType = backupTypeStr
+	}
+	if readDto.ArchiveCompressionFormat != nil {
+		archiveCompressionFormatStr := readDto.ArchiveCompressionFormat.String()
+		backupJobModel.ArchiveCompressionFormat = archiveCompressionFormatStr
+	}
+	if readDto.LastRunStatus != nil {
+		lastRunStatusStr := readDto.LastRunStatus.String()
+		backupJobModel.LastRunStatus = &lastRunStatusStr
+	}
+
+	dbQuery := repo.persistentDbSvc.Handler.Model(backupJobModel).Where(&backupJobModel)
+	if readDto.LastRunBeforeAt != nil {
+		dbQuery = dbQuery.Where("last_run_at < ?", readDto.LastRunBeforeAt.GetAsGoTime())
+	}
+	if readDto.LastRunAfterAt != nil {
+		dbQuery = dbQuery.Where("last_run_at > ?", readDto.LastRunAfterAt.GetAsGoTime())
+	}
+	if readDto.NextRunBeforeAt != nil {
+		dbQuery = dbQuery.Where("next_run_at < ?", readDto.NextRunBeforeAt.GetAsGoTime())
+	}
+	if readDto.NextRunAfterAt != nil {
+		dbQuery = dbQuery.Where("next_run_at > ?", readDto.NextRunAfterAt.GetAsGoTime())
+	}
+	if readDto.CreatedBeforeAt != nil {
+		dbQuery = dbQuery.Where("created_at < ?", readDto.CreatedBeforeAt.GetAsGoTime())
+	}
+	if readDto.CreatedAfterAt != nil {
+		dbQuery = dbQuery.Where("created_at > ?", readDto.CreatedAfterAt.GetAsGoTime())
+	}
+
+	paginatedDbQuery, responsePagination, err := dbHelper.PaginationQueryBuilder(
+		dbQuery, readDto.Pagination,
+	)
+	if err != nil {
+		return responseDto, errors.New("PaginationQueryBuilderError: " + err.Error())
+	}
+
+	backupJobModels := []dbModel.BackupJob{}
+	err = paginatedDbQuery.Find(&backupJobModels).Error
+	if err != nil {
+		return responseDto, errors.New("FindBackupJobsError: " + err.Error())
+	}
+
+	for _, backupJobModel := range backupJobModels {
+		backupJobEntity, err := backupJobModel.ToEntity()
+		if err != nil {
+			slog.Debug(
+				"ModelToEntityError",
+				slog.Uint64("id", backupJobModel.ID),
+				slog.Any("error", err),
+			)
+			continue
+		}
+		backupJobEntities = append(backupJobEntities, backupJobEntity)
+	}
+
+	return dto.ReadBackupJobsResponse{
+		Pagination: responsePagination,
+		Jobs:       backupJobEntities,
+	}, nil
+}
