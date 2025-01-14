30e9f0d8e8f0d2be29242425139ac128c6ddb409
ntorga <northontorga@gmail.com>
2025-01-14 14:34:07 +0000

    refactor: gen pass helper

--- src/infra/helper/genPass.go
+++ src/infra/helper/genPass.go
@@ -1,18 +1,70 @@
 package infraHelper
 
 import (
-	"crypto/rand"
-	"math/big"
+	"math/rand"
+	"strings"
 )
 
+func oneCharCharsetGuarantor(
+	originalString []byte,
+	charset string,
+) []byte {
+	if strings.ContainsAny(string(originalString), charset) {
+		return originalString
+	}
+
+	randomStringIndex := rand.Intn(len(originalString))
+	isFirstChar := randomStringIndex == 0
+	if isFirstChar {
+		randomStringIndex++
+	}
+	isLastChar := randomStringIndex == len(originalString)-1
+	if isLastChar {
+		randomStringIndex--
+	}
+	if randomStringIndex >= len(originalString) {
+		randomStringIndex = len(originalString) - 1
+	}
+
+	randomCharsetIndex := rand.Intn(len(charset))
+	originalString[randomStringIndex] = charset[randomCharsetIndex]
+
+	return originalString
+}
+
 func GenPass(length int) string {
-	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	charsetLen := big.NewInt(int64(len(charset)))
+	lowercaseAlphabetCharset := "abcdefghijklmnopqrstuvwxyz"
+	uppercaseAlphabetCharset := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numericCharset := "0123456789"
+	alphanumericCharset := lowercaseAlphabetCharset + uppercaseAlphabetCharset + numericCharset
+	symbolCharset := "!@#$%^&*()_+"
 
 	pass := make([]byte, length)
+	previousCharset := alphanumericCharset
+	currentCharset := alphanumericCharset
+
 	for i := 0; i < length; i++ {
-		randomIndex, _ := rand.Int(rand.Reader, charsetLen)
-		pass[i] = charset[randomIndex.Int64()]
+		currentCharset = alphanumericCharset
+
+		if previousCharset != symbolCharset {
+			nextCharsetShouldUseSymbol := rand.Float32() < 0.1
+			isTipChar := i == 0 || i == length-1
+			if nextCharsetShouldUseSymbol && !isTipChar {
+				currentCharset = symbolCharset
+			}
+		}
+		currentCharsetLen := len(currentCharset)
+
+		randomCharsetIndex := rand.Intn(currentCharsetLen)
+		pass[i] = currentCharset[randomCharsetIndex]
+		previousCharset = currentCharset
+	}
+
+	if length > 4 {
+		pass = oneCharCharsetGuarantor(pass, lowercaseAlphabetCharset)
+		pass = oneCharCharsetGuarantor(pass, uppercaseAlphabetCharset)
+		pass = oneCharCharsetGuarantor(pass, numericCharset)
+		pass = oneCharCharsetGuarantor(pass, "!@#$%^&*()_+")
 	}
 
 	return string(pass)
