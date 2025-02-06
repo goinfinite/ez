cf4dff6d02f743e384d99bd2a01d764f7a21c1e1
ntorga <northontorga@gmail.com>
2025-02-06 16:42:44 +0000

    fix: change task archive id to str

--- src/domain/valueObject/backupTaskArchiveId.go
+++ src/domain/valueObject/backupTaskArchiveId.go
@@ -2,26 +2,19 @@ package valueObject
 
 import (
 	"errors"
-	"strconv"
-
-	voHelper "github.com/goinfinite/ez/src/domain/valueObject/helper"
 )
 
-type BackupTaskArchiveId uint64
+type BackupTaskArchiveId string
 
 func NewBackupTaskArchiveId(value interface{}) (BackupTaskArchiveId, error) {
-	archiveId, err := voHelper.InterfaceToUint64(value)
+	archiveId, err := NewHash(value)
 	if err != nil {
-		return 0, errors.New("InvalidBackupTaskArchiveId")
+		return "", errors.New("InvalidBackupTaskArchiveId")
 	}
 
 	return BackupTaskArchiveId(archiveId), nil
 }
 
-func (vo BackupTaskArchiveId) Uint64() uint64 {
-	return uint64(vo)
-}
-
 func (vo BackupTaskArchiveId) String() string {
-	return strconv.FormatUint(uint64(vo), 10)
+	return string(vo)
 }
