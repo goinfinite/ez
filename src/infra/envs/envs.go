63e306b42450781c948ef5e1aec730f009afc181
ntorga <northontorga@gmail.com>
2025-02-21 15:54:14 +0000

    fix: add backup prefix to tasks dir env

--- src/infra/envs/envs.go
+++ src/infra/envs/envs.go
@@ -1,13 +1,13 @@
 package infraEnvs
 
 const (
-	InfiniteEzVersion    string = "0.1.0"
-	InfiniteEzMainDir    string = "/var/infinite"
-	MarketplaceDir       string = InfiniteEzMainDir + "/marketplace"
-	InfiniteEzBinary     string = InfiniteEzMainDir + "/ez"
-	AccessTokenCookieKey string = "control-access-token"
-	UserDataDirectory    string = "/var/data"
-	BackupCronFilePath   string = "/etc/cron.d/ez-backup"
-	NobodyDataDirectory  string = UserDataDirectory + "/nobody"
-	RestoreTaskTmpDir    string = NobodyDataDirectory + "/tasks-restores-tmp"
+	InfiniteEzVersion       string = "0.1.0"
+	InfiniteEzMainDir       string = "/var/infinite"
+	MarketplaceDir          string = InfiniteEzMainDir + "/marketplace"
+	InfiniteEzBinary        string = InfiniteEzMainDir + "/ez"
+	AccessTokenCookieKey    string = "control-access-token"
+	UserDataDirectory       string = "/var/data"
+	BackupCronFilePath      string = "/etc/cron.d/ez-backup"
+	NobodyDataDirectory     string = UserDataDirectory + "/nobody"
+	RestoreBackupTaskTmpDir string = NobodyDataDirectory + "/backup/tasks/restore"
 )
