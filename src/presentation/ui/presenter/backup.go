8a1731afa115484d8153c1116716b15716e3407d
ntorga <northontorga@gmail.com>
2025-02-26 22:14:11 +0000

    feat: add ReadTasks()

--- src/presentation/ui/presenter/backup.go
+++ src/presentation/ui/presenter/backup.go
@@ -1,9 +1,13 @@
 package presenter
 
 import (
+	"log/slog"
+	"maps"
 	"net/http"
 
+	"github.com/goinfinite/ez/src/domain/dto"
 	"github.com/goinfinite/ez/src/infra/db"
+	"github.com/goinfinite/ez/src/presentation/service"
 	uiHelper "github.com/goinfinite/ez/src/presentation/ui/helper"
 	"github.com/goinfinite/ez/src/presentation/ui/page"
 	"github.com/labstack/echo/v4"
@@ -27,11 +31,43 @@ func NewBackupPresenter(
 	}
 }
 
+func (presenter *BackupPresenter) ReadTasks(
+	echoContext echo.Context,
+	backupService *service.BackupService,
+) (tasksResponseDto dto.ReadBackupTasksResponse) {
+	paginationMap := uiHelper.PaginationParser(echoContext, "task", "id")
+	requestParamsMap := uiHelper.ReadRequestParser(echoContext, "task", dto.ReadBackupTasksRequest{})
+	serviceRequestBody := paginationMap
+	maps.Copy(serviceRequestBody, requestParamsMap)
+
+	readBackupTasksServiceOutput := backupService.ReadTask(serviceRequestBody)
+	if readBackupTasksServiceOutput.Status != service.Success {
+		slog.Debug("ReadBackupTasksFailure", slog.Any("serviceOutput", readBackupTasksServiceOutput))
+		return tasksResponseDto
+	}
+
+	var assertOk bool
+	tasksResponseDto, assertOk = readBackupTasksServiceOutput.Body.(dto.ReadBackupTasksResponse)
+	if !assertOk {
+		slog.Debug("AssertBackupTasksResponseFailure")
+		return tasksResponseDto
+	}
+
+	return tasksResponseDto
+}
+
 func (presenter *BackupPresenter) Handler(c echo.Context) (err error) {
+	backupService := service.NewBackupService(
+		presenter.persistentDbSvc, presenter.trailDbSvc,
+	)
+
+	backupTasksResponseDto := presenter.ReadTasks(c, backupService)
+
 	pageContent := page.BackupIndex(
-		page.BackupTasksInputDto{},
-		page.BackupJobsInputDto{},
-		page.BackupDestinationsInputDto{},
+		backupTasksResponseDto,
+		dto.ReadBackupTaskArchivesResponse{},
+		dto.ReadBackupJobsResponse{},
+		dto.ReadBackupDestinationsResponse{},
 	)
 
 	return uiHelper.Render(c, pageContent, http.StatusOK)
