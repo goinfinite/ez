27f73e4630df87cfa7d6d0073f3fc3b7a10df9df
ntorga <northontorga@gmail.com>
2025-03-03 20:09:10 +0000

    fix: remove omitempty from read dto and pagination

--- src/domain/dto/pagination.go
+++ src/domain/dto/pagination.go
@@ -5,9 +5,9 @@ import "github.com/goinfinite/ez/src/domain/valueObject"
 type Pagination struct {
 	PageNumber    uint32                               `json:"pageNumber"`
 	ItemsPerPage  uint16                               `json:"itemsPerPage"`
-	SortBy        *valueObject.PaginationSortBy        `json:"sortBy,omitempty"`
-	SortDirection *valueObject.PaginationSortDirection `json:"sortDirection,omitempty"`
-	LastSeenId    *valueObject.PaginationLastSeenId    `json:"lastSeenId,omitempty"`
-	PagesTotal    *uint32                              `json:"pagesTotal,omitempty"`
-	ItemsTotal    *uint64                              `json:"itemsTotal,omitempty"`
+	SortBy        *valueObject.PaginationSortBy        `json:"sortBy"`
+	SortDirection *valueObject.PaginationSortDirection `json:"sortDirection"`
+	LastSeenId    *valueObject.PaginationLastSeenId    `json:"lastSeenId"`
+	PagesTotal    *uint32                              `json:"pagesTotal"`
+	ItemsTotal    *uint64                              `json:"itemsTotal"`
 }
--- src/domain/dto/readBackupTasks.go
+++ src/domain/dto/readBackupTasks.go
@@ -7,19 +7,19 @@ import (
 
 type ReadBackupTasksRequest struct {
 	Pagination        Pagination                           `json:"pagination"`
-	TaskId            *valueObject.BackupTaskId            `json:"taskId,omitempty"`
-	AccountId         *valueObject.AccountId               `json:"accountId,omitempty"`
-	JobId             *valueObject.BackupJobId             `json:"jobId,omitempty"`
-	DestinationId     *valueObject.BackupDestinationId     `json:"destinationId,omitempty"`
-	TaskStatus        *valueObject.BackupTaskStatus        `json:"taskStatus,omitempty"`
-	RetentionStrategy *valueObject.BackupRetentionStrategy `json:"retentionStrategy,omitempty"`
-	ContainerId       *valueObject.ContainerId             `json:"containerId,omitempty"`
-	StartedBeforeAt   *valueObject.UnixTime                `json:"startedBeforeAt,omitempty"`
-	StartedAfterAt    *valueObject.UnixTime                `json:"startedAfterAt,omitempty"`
-	FinishedBeforeAt  *valueObject.UnixTime                `json:"finishedBeforeAt,omitempty"`
-	FinishedAfterAt   *valueObject.UnixTime                `json:"finishedAfterAt,omitempty"`
-	CreatedBeforeAt   *valueObject.UnixTime                `json:"createdBeforeAt,omitempty"`
-	CreatedAfterAt    *valueObject.UnixTime                `json:"createdAfterAt,omitempty"`
+	TaskId            *valueObject.BackupTaskId            `json:"taskId"`
+	AccountId         *valueObject.AccountId               `json:"accountId"`
+	JobId             *valueObject.BackupJobId             `json:"jobId"`
+	DestinationId     *valueObject.BackupDestinationId     `json:"destinationId"`
+	TaskStatus        *valueObject.BackupTaskStatus        `json:"taskStatus"`
+	RetentionStrategy *valueObject.BackupRetentionStrategy `json:"retentionStrategy"`
+	ContainerId       *valueObject.ContainerId             `json:"containerId"`
+	StartedBeforeAt   *valueObject.UnixTime                `json:"startedBeforeAt"`
+	StartedAfterAt    *valueObject.UnixTime                `json:"startedAfterAt"`
+	FinishedBeforeAt  *valueObject.UnixTime                `json:"finishedBeforeAt"`
+	FinishedAfterAt   *valueObject.UnixTime                `json:"finishedAfterAt"`
+	CreatedBeforeAt   *valueObject.UnixTime                `json:"createdBeforeAt"`
+	CreatedAfterAt    *valueObject.UnixTime                `json:"createdAfterAt"`
 }
 
 type ReadBackupTasksResponse struct {
