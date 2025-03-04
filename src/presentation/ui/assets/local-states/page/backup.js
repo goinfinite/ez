b06b5ec4f1dd7473e6840379603b34cc00541d3e
ntorga <northontorga@gmail.com>
2025-03-04 18:49:56 +0000

    feat: init create backup task archive form

--- src/presentation/ui/assets/local-states/page/backup.js
+++ src/presentation/ui/assets/local-states/page/backup.js
@@ -4,5 +4,37 @@ document.addEventListener("alpine:init", () => {
     backupFeatureTabSelected: "tasks",
   }));
 
-  Alpine.data("backupTasks", () => ({}));
+  Alpine.data("backupTasks", () => ({
+    // Primary State
+    taskEntity: {},
+    createTaskArchive: {},
+    resetPrimaryState() {
+      this.taskEntity = {};
+      this.createTaskArchive = {};
+    },
+    updateTaskEntity(taskId) {
+      this.taskEntity = JSON.parse(
+        document.getElementById("backupTaskEntity_" + taskId).textContent
+      );
+    },
+    init() {
+      this.resetPrimaryState();
+    },
+
+    // Auxiliary State
+    isCreateTaskArchiveModalOpen: false,
+
+    openCreateTaskArchiveModal(taskId) {
+      this.updateTaskEntity(taskId);
+      this.createTaskArchive = {
+        taskId: taskId,
+        containerAccountIds: [],
+      };
+      this.isCreateTaskArchiveModalOpen = true;
+    },
+    closeCreateTaskArchiveModal() {
+      this.resetPrimaryState();
+      this.isCreateTaskArchiveModalOpen = false;
+    },
+  }));
 });
