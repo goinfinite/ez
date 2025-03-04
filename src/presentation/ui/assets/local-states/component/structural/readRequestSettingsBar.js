8ac4da7da59788fc082ee300e7e0daf56ca4c63a
ntorga <northontorga@gmail.com>
2025-03-04 13:20:08 +0000

    fix: use watch for pagination instead of event listener

--- src/presentation/ui/assets/local-states/component/structural/readRequestSettingsBar.js
+++ src/presentation/ui/assets/local-states/component/structural/readRequestSettingsBar.js
@@ -8,41 +8,7 @@ function InitReadRequestSettingsLocalState(
       pagination: {},
       filters: {},
 
-      readQueryParams() {
-        queryParams = new URLSearchParams();
-        queryParams.set(componentId + "PageNumber", this.pagination.pageNumber);
-        queryParams.set(
-          componentId + "ItemsPerPage",
-          this.pagination.itemsPerPage
-        );
-
-        for (let [filterKey, filterValue] of Object.entries(this.filters)) {
-          if (filterValue === null) {
-            continue;
-          }
-
-          filterValue = filterValue.trim();
-          if (filterValue === "") {
-            continue;
-          }
-          const filterKeyPascalCase =
-            filterKey.charAt(0).toUpperCase() + filterKey.slice(1);
-          queryParams.set(componentId + filterKeyPascalCase, filterValue);
-        }
-
-        return queryParams.toString();
-      },
-
-      reloadRecords() {
-        htmx.ajax("GET", uiEndpoint + "?" + this.readQueryParams(), {
-          select: recordsDisplayElementId,
-          target: recordsDisplayElementId,
-          indicator: "#loading-overlay",
-          swap: "outerHTML transition:true",
-        });
-      },
-
-      init() {
+      resetState() {
         const readRequestDto = JSON.parse(
           document.getElementById(componentId + "ReadRequestDto").textContent
         );
@@ -57,13 +23,52 @@ function InitReadRequestSettingsLocalState(
         this.pagination = JSON.parse(
           document.getElementById(componentId + "PaginationDto").textContent
         );
+      },
 
-        document.addEventListener(
-          "update:" + componentId + "-pagination",
-          () => {
-            this.reloadRecords();
-          }
+      readQueryParams() {
+        queryParams = new URLSearchParams();
+        queryParams.set(componentId + "PageNumber", this.pagination.pageNumber);
+        queryParams.set(
+          componentId + "ItemsPerPage",
+          this.pagination.itemsPerPage
         );
+
+        for (let [filterKey, filterValue] of Object.entries(this.filters)) {
+          if (filterValue === null) {
+            continue;
+          }
+
+          if (
+            typeof filterValue === "string" ||
+            filterValue instanceof String
+          ) {
+            filterValue = filterValue.trim();
+          }
+          if (filterValue === "") {
+            continue;
+          }
+          const filterKeyPascalCase =
+            filterKey.charAt(0).toUpperCase() + filterKey.slice(1);
+          queryParams.set(componentId + filterKeyPascalCase, filterValue);
+        }
+
+        return queryParams.toString();
+      },
+
+      reloadRecordsDisplay() {
+        htmx.ajax("GET", uiEndpoint + "?" + this.readQueryParams(), {
+          select: recordsDisplayElementId,
+          target: recordsDisplayElementId,
+          indicator: "#loading-overlay",
+          swap: "outerHTML transition:true",
+        });
+      },
+
+      init() {
+        this.resetState();
+        this.$watch("pagination", () => {
+          this.reloadRecordsDisplay();
+        });
       },
     }));
   });
