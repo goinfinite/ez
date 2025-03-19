6529f1ca5da9402573969dfe9cfe40df77cba816
ntorga <northontorga@gmail.com>
2025-03-19 18:37:52 +0000

    feat: update job after tasks delete

--- src/domain/useCase/deleteBackupTask.go
+++ src/domain/useCase/deleteBackupTask.go
@@ -23,12 +23,44 @@ func DeleteBackupTask(
 
 	err = backupCmdRepo.DeleteTask(deleteDto)
 	if err != nil {
-		slog.Error("DeleteBackupTaskInfraError", slog.String("error", err.Error()))
+		slog.Error(
+			"DeleteBackupTaskInfraError",
+			slog.String("error", err.Error()),
+			slog.String("taskId", taskEntity.TaskId.String()),
+		)
 		return errors.New("DeleteBackupTaskError")
 	}
 
 	NewCreateSecurityActivityRecord(activityRecordCmdRepo).
 		DeleteBackupTask(deleteDto, taskEntity.AccountId)
+
+	jobEntity, err := backupQueryRepo.ReadFirstJob(
+		dto.ReadBackupJobsRequest{JobId: &taskEntity.JobId},
+	)
+	if err != nil {
+		slog.Error(
+			"BackupJobNotFound",
+			slog.String("error", err.Error()),
+			slog.String("jobId", taskEntity.JobId.String()),
+		)
+		return nil
+	}
+
+	newJobTasksCount := jobEntity.TasksCount - 1
+	newJobTotalSpaceUsageBytes := jobEntity.TotalSpaceUsageBytes
+	if taskEntity.SizeBytes != nil {
+		newJobTotalSpaceUsageBytes -= *taskEntity.SizeBytes
+	}
+
+	err = backupCmdRepo.UpdateJob(dto.UpdateBackupJob{
+		JobId:                taskEntity.JobId,
+		TasksCount:           &newJobTasksCount,
+		TotalSpaceUsageBytes: &newJobTotalSpaceUsageBytes,
+	})
+	if err != nil {
+		slog.Error("UpdateBackupJobStatsInfraError", slog.String("error", err.Error()))
+		return errors.New("UpdateBackupJobStatsInfraError")
+	}
 
 	return nil
 }
