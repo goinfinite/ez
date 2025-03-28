3630f2b5cb77b22cc295ae77c1f37e06a6a4ffd4
polillomm <matheus_polillo@proton.me>
2025-03-28 21:55:29 +0000

    feat: implements account delete modal

--- src/presentation/ui/assets/local-states/page/accounts.js
+++ src/presentation/ui/assets/local-states/page/accounts.js
@@ -1,4 +1,35 @@
-document.addEventListener("alpine:init", () => {
-    Alpine.data("accounts", () => ({
-    }));
-});
+document.addEventListener('alpine:init', () => {
+    Alpine.data('accounts', () => ({
+        // Primary States
+        accountEntity: {},
+        resetPrimaryStates() {
+            this.accountEntity = {}
+        },
+        init() {
+            this.resetPrimaryStates()
+        },
+
+        // Modal States
+        isDeleteAccountModalOpen: false,
+        openDeleteAccountModal(accountId) {
+            this.resetPrimaryStates()
+
+            this.accountEntity = JSON.parse(
+                document.getElementById("accountEntity_" + accountId).textContent
+            )
+
+            this.isDeleteAccountModalOpen = true
+        },
+        closeDeleteAccountModal() {
+            this.isDeleteAccountModalOpen = false
+        },
+        deleteAccount() {
+            htmx.ajax(
+                "DELETE",
+                "/api/v1/account/" + this.accountEntity.id + "/",
+                { swap: "none" },
+            ).then(() => this.$dispatch("delete:account"))
+            this.closeDeleteAccountModal()
+        },
+    }))
+})
