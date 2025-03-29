14363a10fdd8466bbe8cbf659d24b37c1196580f
polillomm <matheus_polillo@proton.me>
2025-03-29 20:48:03 +0000

    feat: implements create account and update account quota modals using the same form

--- src/presentation/ui/assets/local-states/page/accounts.js
+++ src/presentation/ui/assets/local-states/page/accounts.js
@@ -3,18 +3,52 @@ document.addEventListener('alpine:init', () => {
         accountEntity: {},
         accountApiKey: "",
         resetPrimaryStates() {
-            this.accountEntity = {}
+            this.accountEntity = {
+                id: "",
+                username: "",
+                password: "",
+                quota: {
+                    cpuCores: 0,
+                    memoryGibibytes: 0,
+                    memoryMebibytes: 0,
+                    storageGibibytes: 0,
+                    storageMebibytes: 0,
+                    storageInodes: 0,
+                    storagePerformanceUnits: 1,
+                },
+            }
             this.accountApiKey = ""
         },
         updateAccountEntity(accountId) {
             this.accountEntity = JSON.parse(
                 document.getElementById("accountEntity_" + accountId).textContent
             )
+            delete this.accountEntity.password
         },
         init() {
             this.resetPrimaryStates()
         },
 
+        isMemoryGibibyteSelector: true,
+        isStorageGibibyteSelection: true,
+
+        isCreateAccountModalOpen: false,
+        openCreateAccountModal() {
+            this.resetPrimaryStates()
+            this.isCreateAccountModalOpen = true
+        },
+        closeCreateAccountModal() {
+            this.isCreateAccountModalOpen = false
+        },
+        isUpdateAccountQuotaModalOpen: false,
+        openUpdateAccountQuotaModal(accountId) {
+            this.resetPrimaryStates()
+            this.updateAccountEntity(accountId)
+            this.isUpdateAccountQuotaModalOpen = true
+        },
+        closeUpdateAccountQuotaModal() {
+            this.isUpdateAccountQuotaModalOpen = false
+        },
         isUpdatePasswordModalOpen: false,
         openUpdatePasswordModal(accountId) {
             this.resetPrimaryStates()
