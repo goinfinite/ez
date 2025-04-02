45aa521479c60a98ababebc9c40e37239b520943
polillomm <matheus_polillo@proton.me>
2025-04-02 04:42:35 +0000

    feat: implements delete mapping modal

--- src/presentation/ui/assets/local-states/page/mappings.js
+++ src/presentation/ui/assets/local-states/page/mappings.js
@@ -1,11 +1,38 @@
 document.addEventListener("alpine:init", () => {
     Alpine.data("mappings", () => ({
+        mappingEntity: {},
+        resetPrimaryStates() {
+            this.mappingEntity = {}
+        },
+        init() {
+            this.resetPrimaryStates()
+        },
+
         isCreateMappingModalOpen: false,
         openCreateMappingModal() {
+            this.resetPrimaryStates()
             this.isCreateMappingModalOpen = true
         },
         closeCreateMappingModal() {
             this.isCreateMappingModalOpen = false
+        },
+        isDeleteMappingModalOpen: false,
+        openDeleteMappingModal(mappingId, mappingHostname) {
+            this.resetPrimaryStates()
+            this.mappingEntity.id = mappingId
+            this.mappingEntity.hostname = mappingHostname
+            this.isDeleteMappingModalOpen = true
+        },
+        closeDeleteMappingModal() {
+            this.isDeleteMappingModalOpen = false
+        },
+        deleteMapping() {
+            htmx
+                .ajax("DELETE", "/api/v1/mapping/" + this.mappingEntity.id + "/", {
+                    swap: "none",
+                })
+                .then(() => this.$dispatch("refresh:mappings-table"))
+            this.closeDeleteMappingModal()
         },
     }))
 })
